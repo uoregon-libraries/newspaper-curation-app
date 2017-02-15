dd1d6687153ec12b0fcc6ecf18dabe559c5ca014
Jeremy Echols <jechols@uoregon.edu>
2017-02-15 09:45:12 -0800

    Create fileutil.IsDir

---
+++ src/fileutil/fileutil.go
@@ -0,0 +1,14 @@
+// package fileutil holds various things this suite needs for easier
+// identification and processing of files
+package fileutil
+
+import "os"
+
+// IsDir returns true if the given path exists and is a directory
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
