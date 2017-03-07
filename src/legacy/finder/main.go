f8957bdbdafd9a9a0b412ccca3dcc88ee21134ca
Jeremy Echols <jechols@uoregon.edu>
2017-03-07 11:27:40 -0800

    Use new issueSearchKey type and parsing
    Takes parsing and validation responsibility out of main's getConf
    function so it's a bit easier to read, and validation is more useful

--- src/legacy/finder/main.go
+++ src/legacy/finder/main.go
@@ -13,37 +13,37 @@ import (
 	"log"
 	"os"
 	"strings"
-	"time"
 	"wordutils"
 
 	"github.com/jessevdk/go-flags"
 )
 
-type issueKey struct {
-	LCCN string
-	Date string
-}
-
 var Conf *config.Config
-var IssueKeys []*issueKey
+var issueSearchKeys []*issueSearchKey
 
 // Command-line options
 var opts struct {
 	ConfigFile string   `short:"c" long:"config" description:"path to P2C config file" required:"true"`
 	IssueList  string   `long:"issue-list" description:"path to file containing list of newline-separated issue keys"`
-	IssueKeys  []string `long:"issue-key" description:"single issue key to process, e.g., 'sn12345678/19051231'"`
+	IssueKeys  []string `long:"issue-key" description:"single issue key to process, e.g., 'sn12345678/1905123101'"`
 }
 
 var p *flags.Parser
 
 func usageFail(format string, args ...interface{}) {
 	fmt.Fprint(os.Stderr, wordutils.Wrap(fmt.Sprintf(format, args...), 80))
-	fmt.Fprint(os.Stderr, "\n\n")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr)
 	p.WriteHelp(os.Stderr)
-	fmt.Fprint(os.Stderr, "\n")
+	fmt.Fprintln(os.Stderr)
 	fmt.Fprint(os.Stderr, wordutils.Wrap("At least one of --issue-list or " +
 		"--issue-key must be specified.  If both are specified, --issue-key will " +
-		"be ignored.  Note that --issue-key may be specified multiple times.\n", 80))
+		"be ignored.  Note that --issue-key may be specified multiple times.", 80))
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprint(os.Stderr, wordutils.Wrap("Issue keys MUST be formatted as " +
+		`LCCN/YYYY[MM][DD][EE].  The full LCCN and year are mandatory.`, 80))
+	fmt.Fprintln(os.Stderr)
 	os.Exit(1)
 }
 
@@ -85,31 +85,22 @@ func getConf() {
 		if ik == "" {
 			continue
 		}
-		var parts = strings.Split(ik, "/")
-		if len(parts) != 2 {
-			usageFail("Invalid issue key %#v", ik)
-		}
-		var lccn, date = parts[0], parts[1]
-		if len(lccn) != 10 {
-			usageFail("Invalid LCCN format, %#v (LCCNs must be 10 characters)", lccn)
-		}
 
-		_, err = time.Parse("20060102", date)
+		var searchKey, err = parseSearchKey(ik)
 		if err != nil {
-			usageFail("Invalid date format, %#v (must be YYYYMMDD)", date)
+			usageFail("Invalid issue search key %#v: %s", ik, err)
 		}
-
-		IssueKeys = append(IssueKeys, &issueKey{lccn, date})
+		issueSearchKeys = append(issueSearchKeys, searchKey)
 	}
 
-	if len(IssueKeys) == 0 {
+	if len(issueSearchKeys) == 0 {
 		usageFail("No valid issue keys were found (did you use a blank issue key?)")
 	}
 }
 
 func main() {
 	getConf()
-	for _, ik := range IssueKeys {
-		log.Print(ik)
+	for _, ik := range issueSearchKeys {
+		log.Printf("DEBUG: Searching for issue %#v", ik)
 	}
 }
