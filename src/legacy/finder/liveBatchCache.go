4162945d82056ce78a0b5d510d45c6cca73f21e8
Jeremy Echols <jechols@uoregon.edu>
2017-03-10 13:00:12 -0800

    Add live batch/issue spidering and caching

---
+++ src/legacy/finder/liveBatchCache.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"httpcache"
+	"log"
+	"net/url"
+	"path"
+	"strconv"
+	"time"
+)
+
+// cacheLiveBatchedIssues reads through the JSON from the batch API URL and
+// grabs "next" page until there is no next page.  Each batch is then read from
+// the JSON cache path, or read from the site and cached.  The disk cache
+// speeds up the tool's future runs by only having to request what's been
+// batched since a prior run.
+func cacheLiveBatchedIssues(hostname, cachePath string) error {
+	var batchMetadataList, err = findAllLiveBatches(hostname, cachePath)
+	if err != nil {
+		return fmt.Errorf("unable to load batch list from %#v: %s", hostname, err)
+	}
+
+	// We (slightly) throttle batch JSON requests as there can be a few hundred of these
+	var c = httpcache.NewClient(cachePath, 50)
+	for _, batchMetadata := range batchMetadataList {
+		var batch, err = ParseBatchname(batchMetadata.Name)
+		if err != nil {
+			return fmt.Errorf("invalid live batch name %#v: %s", batchMetadata.Name, err)
+		}
+
+		var issueMetadataList []*issueMetadata
+		issueMetadataList, err = findBatchedIssueMetadata(c, batchMetadata.URL)
+		if err != nil {
+			return fmt.Errorf("unable to load live issues from %#v: %s", batchMetadata.URL, err)
+		}
+		err = cacheLiveIssuesFromMetadata(batch, issueMetadataList)
+		if err != nil {
+			return fmt.Errorf("unable to load live issues from %#v: %s", batchMetadata.URL, err)
+		}
+	}
+
+	return nil
+}
+
+func cacheLiveIssuesFromMetadata(batch *Batch, issueMetadataList []*issueMetadata) error {
+	for _, meta := range issueMetadataList {
+		var title = getTitleFromIssueMetadata(meta)
+		var dt, err = time.Parse("2006-01-02", meta.Date)
+		if err != nil {
+			return fmt.Errorf("invalid date for issue %#v: %s", meta, err)
+		}
+
+		// We can determine edition from the issue URL, as it always ends in "ed-?.json"
+		var base = path.Base(meta.URL)
+		var editionString = base[3:]
+		editionString = editionString[:len(editionString)-5]
+		var edition int
+		edition, err = strconv.Atoi(editionString)
+		if err != nil {
+			return fmt.Errorf("invalid edition (%#v) for issue %#v", editionString, meta)
+		}
+
+		var issue = title.AppendIssue(dt, edition)
+		issue.Batch = batch
+		cacheIssue(issue, meta.URL)
+	}
+
+	return nil
+}
+
+// getTitleFromIssueMetadata finds or creates a title from the given issue
+// metadata.  If a new title is created, it's stored in the title lookup.
+func getTitleFromIssueMetadata(meta *issueMetadata) *Title {
+	var base = path.Base(meta.Title.URL)
+	var titleLCCN = base[:len(base)-5]
+	var title = titleLookup[titleLCCN]
+	if title == nil {
+		title = &Title{LCCN: titleLCCN}
+		titleLookup[titleLCCN] = title
+	}
+	return title
+}
+
+func findBatchedIssueMetadata(c *httpcache.Client, batchURL string) ([]*issueMetadata, error) {
+	log.Printf("DEBUG - reading issues list for %#v", batchURL)
+	var request = httpcache.AutoRequest(batchURL, "batches")
+	var contents, err = c.GetCachedBytes(request)
+	if err != nil {
+		return nil, fmt.Errorf("unable to GET %#v: %s", batchURL, err)
+	}
+
+	var batch *batchJSON
+	batch, err = parseBatchJSON(contents)
+	if err != nil {
+		return nil, fmt.Errorf("invalid JSON in %#v: %s", batchURL, err)
+	}
+
+	return batch.Issues, nil
+}
+
+// findAllLiveBatches hits the web server to request the full list of every
+// known batch.  Results are stored in the cache path for debugging, but in
+// this case the cache is never actually read, just written.  The returned
+// structures are the aggregated batch metadata objects found after traversing
+// all pages of batches.
+func findAllLiveBatches(hostname, cachePath string) ([]*batchMetadata, error) {
+	// We don't bother throttling because there won't be more than a handful of
+	// batch list pages
+	var c = httpcache.NewClient(cachePath, 0)
+
+	var apiURL, err = url.Parse(hostname)
+	if err != nil {
+		return nil, err
+	}
+	apiURL.Path = "batches.json"
+	var batchList = &batchesListJSON{Next: apiURL.String()}
+
+	var page int
+	var batchMetadataList []*batchMetadata
+	var request = &httpcache.Request{Subdirectory: "batch-list", Extension: "json"}
+
+	for batchList.Next != "" {
+		page++
+		request.Filename = fmt.Sprintf("page-%d", page)
+		request.URL = batchList.Next
+
+		log.Printf("DEBUG - reading batches list page %d: %#v", page, request.URL)
+		var contents, err = c.ForceGetBytes(request)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create and deserialize into a new structure
+		batchList = &batchesListJSON{}
+		err = json.Unmarshal(contents, batchList)
+		if err != nil {
+			return nil, err
+		}
+		for _, b := range batchList.Batches {
+			batchMetadataList = append(batchMetadataList, b)
+		}
+	}
+
+	return batchMetadataList, nil
+}
--- src/legacy/finder/search.go
+++ src/legacy/finder/search.go
@@ -7,24 +7,33 @@ import (
 	"time"
 )
 
-// issueMap links a textual issue key to all known issue locations
-type issueMap map[string][]string
+// issueMap links a textual issue key to the Issue object
+type issueMap map[string][]*Issue
 
-// mapIssuePath shortcuts the process of getting an issue's key and storing its
-// filesystem path in the IssueMap
-func (im issueMap) mapIssuePath(i *Issue, path string) {
-	path = filepath.Clean(path)
-	var k = i.Key()
-	var list = im[k]
-	list = append(list, "file://" + path)
-	im[k] = list
-}
+// issueLocMap links a textual issue key to all known issue locations
+type issueLocMap map[string][]string
 
 // titleLookup lets us find titles by LCCN
 var titleLookup = make(map[string]*Title)
 
-// issueLookup lets us find an issue's raw location
+// issueLookup lets us find issues by key
 var issueLookup = make(issueMap)
+
+// issueLocLookup lets us find an issue's raw location
+var issueLocLookup = make(issueLocMap)
+
+// cacheIssue shortcuts the process of getting an issue's key and storing its
+// filesystem path in issueLocLookup and the object in issueLookup
+func cacheIssue(i *Issue, location string) {
+	var k = i.Key()
+	var iList = issueLookup[k]
+	iList = append(iList, i)
+	issueLookup[k] = iList
+
+	var ipList = issueLocLookup[k]
+	ipList = append(ipList, location)
+	issueLocLookup[k] = ipList
+}
 
 // cacheAllIssues calls all the individual cache functions for the myriad of
 // ways we store issue information in the various locations
@@ -37,7 +46,6 @@ func cacheAllIssues() {
 	}
 
 	// TODO: Still need to handle (or decide not to bother handling):
-	// - Live batched issues
 	// - config.PDFIssuesAwaitingDerivatives
 	// - config.PDFPageReviewPath
 	// - config.PDFPagesAwaitingMetadataReview
@@ -79,8 +87,7 @@ func cacheSFTPIssues() error {
 				continue
 			}
 			var issue = title.AppendIssue(dt, 1)
-			issueLookup.mapIssuePath(issue, issuePath)
-			log.Println(issuePath)
+			cacheIssue(issue, issuePath)
 		}
 	}
 
