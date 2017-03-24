de0f1cd5cdd60f5b89ccc2b92ba90a46d06543ce
Jeremy Echols <jechols@uoregon.edu>
2017-03-24 10:15:19 -0700

    Extract "standard issue structure" cache logic

--- src/legacy/finder/filesystem_issue_cache.go
+++ src/legacy/finder/filesystem_issue_cache.go
@@ -4,6 +4,7 @@ import (
 	"fileutil"
 	"log"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -18,6 +19,8 @@ func cacheAllFilesystemIssues() {
 	}
 }
 
+// cacheSFTPIssues is just barely its own special case because unlike the
+// standard structure, there is no "topdir" element in the paths
 func cacheSFTPIssues() error {
 	// First find all titles
 	var titlePaths, err = fileutil.FindDirectories(Conf.MasterPDFUploadPath)
@@ -27,30 +30,59 @@ func cacheSFTPIssues() error {
 
 	// Find all issues next
 	for _, titlePath := range titlePaths {
-		// Make sure we have a legitimate title
-		var titleName = filepath.Base(titlePath)
-		var title = titlesBySFTPDir[titleName]
-		if title == nil {
-			log.Printf("WARNING: Invalid title detected: %s", titleName)
-			continue
-		}
-
-		// In SFTP-land, issues are ALWAYS subdirectories in the format of
-		// YYYY-MM-DD, or else we consider them errors
-		var issuePaths, err = fileutil.FindDirectories(titlePath)
+		err = cacheStandardIssuesForTitle(titlePath)
 		if err != nil {
 			return err
 		}
+	}
 
-		for _, issuePath := range issuePaths {
-			var base = filepath.Base(issuePath)
-			var dt, err = time.Parse("2006-01-02", base)
-			if err != nil {
-				continue
-			}
-			var issue = title.AppendIssue(dt, 1)
-			cacheIssue(issue, issuePath)
+	return nil
+}
+
+// cacheStandardIssuesForTitle finds all issues within the given title's path
+// by looking for YYYY-MM-DD formatted directories.  The path is expected to be
+// "standard", so the last directory element in the path must be an SFTP title
+// name or an LCCN.
+func cacheStandardIssuesForTitle(path string) error {
+	// Make sure we have a legitimate title - we have to check both the SFTP
+	// and LCCN lookups
+	var titleName = filepath.Base(path)
+	var title = titlesBySFTPDir[titleName]
+	if title == nil {
+		title = titlesByLCCN[titleName]
+	}
+
+	// Not having a title is a problem, but not a reason to fail the whole
+	// process, so we log an error while letting the caller continue
+	if title == nil {
+		log.Printf("ERROR: Invalid title detected: %s", titleName)
+		return nil
+	}
+
+	var issuePaths, err = fileutil.FindDirectories(path)
+	if err != nil {
+		return err
+	}
+
+	for _, issuePath := range issuePaths {
+		var base = filepath.Base(issuePath)
+		// To avoid excessive errors, we can skip anything ending in "-error", as
+		// that's currently one way we flag problems
+		if strings.HasSuffix(base, "-error") {
+			continue
 		}
+
+		var dt, err = time.Parse("2006-01-02", base)
+		// Invalid issue directories are sometimes an error and sometimes something
+		// to ignore due to how publishers sometimes name directories, how we flag
+		// directories for review, etc.  We log a warning and move on, and
+		// hopefully someday we have a more elegant approach.
+		if err != nil {
+			log.Printf("WARNING: Invalid issue directory %#v: %s", issuePath, err)
+			continue
+		}
+		var issue = title.AppendIssue(dt, 1)
+		cacheIssue(issue, issuePath)
 	}
 
 	return nil
