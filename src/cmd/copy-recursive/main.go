d699e03b1da55d8e010aa50c811f765d431dcc13
Jeremy Echols <jechols@uoregon.edu>
2017-08-31 13:47:13 -0700

    Test recursive copy

---
+++ src/cmd/copy-recursive/main.go
@@ -0,0 +1,25 @@
+// This is a quick cli for copying directories to verify that
+// fileutil.CopyDirectory works
+
+package main
+
+import (
+	"fileutil"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %q <source directory> <destination directory>\n\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	var err = fileutil.CopyDirectory(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("Fail: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Success")
+}
