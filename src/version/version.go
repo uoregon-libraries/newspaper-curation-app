0d5a13ba36f29537d1ddfb7a9e5962829d06ec9c
Jeremy Echols <jechols@uoregon.edu>
2017-08-31 10:18:20 -0700

    Fix version

--- src/version/version.go
+++ src/version/version.go
@@ -1,6 +1,6 @@
-// package version is just for holding high-level versioning information for
+// Package version is just for holding high-level versioning information for
 // the project as a whole
 package version
 
 // Version is the raw version string
-const Version = "1.3.3"
+const Version = "1.4.0"
