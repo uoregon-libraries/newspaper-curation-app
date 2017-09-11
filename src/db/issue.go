428bca1fd26dacdb56d334e20d6ee0f92a59cde4
Jeremy Echols <jechols@uoregon.edu>
2017-09-11 14:48:06 -0700

    Remove workflow from db.Issue

--- src/db/issue.go
+++ src/db/issue.go
@@ -6,42 +6,7 @@ import (
 	"strings"
 )
 
-// WorkflowStep is an enumeration of where an issue can be in the workflow
-type WorkflowStep int
-
-const (
-	// WSUnknown is the zero-value workflow step, indicating something wasn't
-	// initialized properly
-	WSUnknown WorkflowStep = iota
-
-	// WSPreppingSFTPIssueForMove is set when a born-digital issue is about to be
-	// moved for processing so we can track what happened if it fails to move
-	WSPreppingSFTPIssueForMove
-
-	// WSAwaitingPDFProcessing is for born-digital issues that have just been
-	// moved from their upload location and need to be split, converted to PDF/a,
-	// and moved on for page reordering.
-	WSAwaitingPDFProcessing
-
-	// WSAwaitingManualProcessing is used only for born-digital issues as they
-	// need to be reordered and sometimes have corrections, such as removing
-	// duplicated pages
-	WSAwaitingManualProcessing
-
-	// WSReadyToProcess means the issues are 100% in the control of our
-	// applications, and can have derivatives created, metadata entered, etc.
-	WSReadyToProcess
-
-	// WSReadyToBatch is set up once derivatives have been created, and all the
-	// metadata is entered and reviewed
-	WSReadyToBatch
-)
-
-// Issue contains data about an issue - metadata as well as workflow and
-// location.  This allows us to know where an issue is in the process, what
-// still needs to be done, who currently "owns" it, etc.  It also lets us track
-// on issues that have are waiting for manual processing so we know to check if
-// they've been dealt with.
+// Issue contains metadata about an issue for the various workflow tools' use
 //
 // TODO: Right now this is only used by the Go tools, but eventually the PHP
 // and Python scripts need to use this (or be rewritten) so we are managing
@@ -65,23 +30,6 @@ type Issue struct {
 	EditionLabel  string
 	PageLabelsCSV string
 	PageLabels    []string `sql:"-"`
-
-	/* Workflow data */
-
-	Location         string
-	WorkflowStepInt  int          `sql:"workflow_step"`
-	WorkflowStep     WorkflowStep `sql:"-"`
-	NeedsDerivatives bool
-
-	// Info holds any informational message the end-user may need, but which
-	// doesn't prevent that user from workflow actions.  These messages may have
-	// caused the workflow to stop previously, and are explaining to the user why
-	// a problem occurred, while still allowing the user to try again.
-	Info string
-
-	// Error holds critical errors with the issue; if present, the end-user
-	// cannot perform any actions, as some kind of manual intervention is needed.
-	Error string
 }
 
 // FindIssueByKey looks for an issue in the database that has the given issue key
@@ -114,24 +62,6 @@ func FindIssueByKey(key string) (*Issue, error) {
 	return i, op.Err()
 }
 
-// FindAllAwaitingPDFProcessing finds, deserializes, and returns all issues
-// with a workflow status of WSAwaitingManualProcessing
-func FindAllAwaitingPDFProcessing() ([]*Issue, error) {
-	var op = DB.Operation()
-	op.Dbg = Debug
-	var issues = make([]*Issue, 0)
-	op.Select("issues", &Issue{}).Where("workflow_step = ?", int(WSAwaitingPDFProcessing)).AllObjects(&issues)
-	for _, i := range issues {
-		i.deserialize()
-	}
-	return issues, op.Err()
-}
-
-// NewIssue sets up a structure for storing issue metadata in the database
-func NewIssue(location string) *Issue {
-	return &Issue{Location: location, NeedsDerivatives: true}
-}
-
 // Save creates or updates the Issue in the issues table
 func (i *Issue) Save() error {
 	i.serialize()
@@ -144,12 +74,10 @@ func (i *Issue) Save() error {
 // serialize prepares struct data to work with the database fields better
 func (i *Issue) serialize() {
 	i.PageLabelsCSV = strings.Join(i.PageLabels, ",")
-	i.WorkflowStepInt = (int)(i.WorkflowStep)
 }
 
 // deserialize performs operations necessary to get the database data into a more
 // useful Go structure
 func (i *Issue) deserialize() {
 	i.PageLabels = strings.Split(i.PageLabelsCSV, ",")
-	i.WorkflowStep = (WorkflowStep)(i.WorkflowStepInt)
 }
