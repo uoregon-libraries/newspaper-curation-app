26e7c453ea79ae4812c125a1a71c1c6966f3c365
Jeremy Echols <jechols@uoregon.edu>
2017-09-13 10:42:23 -0700

    Fix page split queue function

--- src/jobs/queue.go
+++ src/jobs/queue.go
@@ -3,7 +3,12 @@ package jobs
 import "db"
 
 // QueuePageSplit creates and queues a page-splitting job with the given data
-func QueuePageSplit(t JobType, id int, path string) error {
-	var j = &db.Job{Type: string(t), ObjectID: id, Location: path, Status: string(JobStatusPending)}
+func QueuePageSplit(issue *db.Issue, path string) error {
+	var j = &db.Job{
+		Type:     string(JobTypePageSplit),
+		ObjectID: issue.ID,
+		Location: path,
+		Status:   string(JobStatusPending),
+	}
 	return j.Save()
 }
