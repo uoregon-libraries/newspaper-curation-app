0e38cea9df9827fc77863d059065530198122d23
Jeremy Echols <jechols@uoregon.edu>
2017-09-26 13:33:50 -0700

    Remove FindAllPendingJobs

--- src/jobs/jobs.go
+++ src/jobs/jobs.go
@@ -3,7 +3,6 @@ package jobs
 import (
 	"db"
 	"logger"
-	"schema"
 )
 
 // JobType represents all possible jobs the system queues and processes
@@ -28,27 +27,6 @@ const (
 	JobStatusFailedDone JobStatus = "failed_done" // Jobs we ignore - e.g., failed jobs which were rerun
 )
 
-// FindAllPendingJobs returns a list of all jobs needing processing
-func FindAllPendingJobs() (processors []Processor) {
-	var dbJobs, err = db.FindJobsByStatus(string(JobStatusPending))
-	for _, ij := range issueJobFindWrapper(dbJobs, err, "find pending jobs") {
-		switch JobType(ij.Type) {
-		case JobTypeSFTPIssueMove:
-			processors = append(processors, &SFTPIssueMover{IssueJob: ij})
-		case JobTypePageSplit:
-			processors = append(processors, &PageSplit{IssueJob: ij})
-		case JobTypeMoveIssueForDerivatives:
-			processors = append(processors, &MoveIssueForDerivatives{IssueJob: ij})
-		case JobTypeMakeDerivatives:
-			processors = append(processors, &MakeDerivatives{IssueJob: ij})
-		default:
-			logger.Error("Unknown job type %q for job id %d", ij.Type, ij.ID)
-		}
-	}
-
-	return
-}
-
 // FindAllFailedJobs returns a list of all jobs which failed; these are not
 // wrapped into IssueJobs or Processors, as failed jobs aren't meant to be
 // reprocessed (though they can be requeued by creating new jobs)
@@ -63,50 +41,4 @@ func FindAllFailedJobs() (jobs []*Job) {
 		jobs = append(jobs, NewJob(dbj))
 	}
 	return
-}
-
-// issueJobFindWrapper takes the response from most job-finding db functions
-// and returns a list of IssueJobs, validating everything as needed and logging
-// Critical errors when any DB operation failed
-//
-// TODO: Remove this and build a db Job converter than switches on the job type
-// to determine exactly what needs to be created, then returns a Processor with
-// all the information set up as needed.
-func issueJobFindWrapper(dbJobs []*db.Job, err error, onErrorMessage string) (issueJobs []*IssueJob) {
-	if err != nil {
-		logger.Critical("Unable to %s: %s", onErrorMessage, err)
-		return
-	}
-
-	for _, dbJob := range dbJobs {
-		var j = dbJobToIssueJob(dbJob)
-		if j == nil {
-			continue
-		}
-		issueJobs = append(issueJobs, j)
-	}
-	return
-}
-
-// dbJobToIssueJob setups up an IssueJob from a database Job, centralizing the
-// common validations and data manipulation
-func dbJobToIssueJob(dbJob *db.Job) *IssueJob {
-	var dbi, err = db.FindIssue(dbJob.ObjectID)
-	if err != nil {
-		logger.Critical("Unable to find issue for job %d: %s", dbJob.ID, err)
-		return nil
-	}
-
-	var si *schema.Issue
-	si, err = dbi.SchemaIssue()
-	if err != nil {
-		logger.Critical("Unable to prepare a schema.Issue for database issue %d: %s", dbi.ID, err)
-		return nil
-	}
-
-	return &IssueJob{
-		Job:     NewJob(dbJob),
-		DBIssue: dbi,
-		Issue:   si,
-	}
 }
