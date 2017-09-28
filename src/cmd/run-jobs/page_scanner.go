a6508162c9101c610cee27aaf07ab7a9d4f476b3
Jeremy Echols <jechols@uoregon.edu>
2017-09-28 14:50:41 -0700

    Create infrastructure for finding scanned issues

--- src/cmd/run-jobs/page_scanner.go
+++ src/cmd/run-jobs/page_scanner.go
@@ -3,15 +3,19 @@ package main
 import (
 	"config"
 	"db"
+	"fileutil"
 	"jobs"
 	"logger"
 	"os"
 	"path/filepath"
 	"regexp"
+	"sort"
+	"strings"
 	"time"
 )
 
 var pdfFilenameRegex = regexp.MustCompile(`(?i:^[0-9]{4}.pdf)`)
+var tiffFilenameRegex = regexp.MustCompile(`(?i:^[0-9]{4}.tiff?)`)
 
 func scanPageReviewIssues(c *config.Config) {
 	var list, err = db.FindIssuesInPageReview()
@@ -24,6 +28,41 @@ func scanPageReviewIssues(c *config.Config) {
 		if issuePagesReady(dbIssue.Location, time.Hour, pdfFilenameRegex) {
 			queueIssueForDerivatives(dbIssue)
 		}
+	}
+}
+
+// scanScannedIssues is a terrible name for the very important process of
+// looking for in-house scanned issues that are valid and ready for processing
+func scanScannedIssues(c *config.Config) {
+	var mocDirs = getScannedMOCDirList(c.ScansAwaitingDerivatives)
+	var lccnDirs []string
+	for _, mocDir := range mocDirs {
+		lccnDirs = append(lccnDirs, getLCCNDirs(mocDir)...)
+	}
+	var dbIssues []*db.Issue
+	for _, lccnDir := range lccnDirs {
+		dbIssues = append(dbIssues, makeScannedDBIssuesFromLCCNDir(lccnDir)...)
+	}
+
+	for _, dbIssue := range dbIssues {
+		// Make sure generic "page ready" logic is good
+		if !issuePagesReady(dbIssue.Location, time.Hour*24, pdfFilenameRegex, tiffFilenameRegex) {
+			continue
+		}
+
+		// Make sure we have exactly the same TIFF and PDF files; we check this in
+		// the derivative processor, but that's more of a backup validation; it's
+		// best to catch errors here so they're easy to fix
+		if !validScanFiles(dbIssue.Location) {
+			continue
+		}
+
+		// Make sure the PDFs' images are at the right DPI
+		if !validScanPDFDPI(dbIssue.Location) {
+			continue
+		}
+
+		queueIssueForDerivatives(dbIssue)
 	}
 }
 
@@ -49,4 +88,101 @@ func queueIssueForDerivatives(dbIssue *db.Issue) {
 
 	// Queue up move to workflow dir
 	jobs.QueueMoveIssueForDerivatives(dbIssue, newDir)
+}
+
+func getScannedMOCDirList(path string) []string {
+	var infos, err = fileutil.ReaddirSorted(path)
+	if err != nil {
+		logger.Error("Unable to read scan directory %q: %s", path, err)
+		return nil
+	}
+
+	var mocDirs []string
+	for _, info := range infos {
+		// We silently skip top-level files, as there seem to often be things like
+		// scan log files generated automatically
+		if !info.IsDir() {
+			continue
+		}
+		var code = filepath.Base(info.Name())
+
+		// We shouldn't have directories that aren't in the system
+		db.Debug = true
+		if !db.ValidMOC(code) {
+			logger.Error("Invalid MARC Org Code directory: %q", info.Name())
+			continue
+		}
+
+		mocDirs = append(mocDirs, filepath.Join(path, info.Name()))
+	}
+
+	return mocDirs
+}
+
+// getLCCNDirs scans the given path for valid LCCN dirs
+func getLCCNDirs(path string) []string {
+	var infos, err = fileutil.ReaddirSorted(path)
+	if err != nil {
+		logger.Error("Unable to scan %q for LCCN dirs: %s", path, err)
+		return nil
+	}
+
+	var lccnDirs []string
+	for _, info := range infos {
+		// We skip top-level files with a warning
+		if !info.IsDir() {
+			logger.Warn("Unexpected file found in LCCN dir scan: %q", info.Name())
+			continue
+		}
+		var lccn = filepath.Base(info.Name())
+
+		// We shouldn't have LCCN directories that aren't in the system
+		if db.LookupTitle(lccn) == nil {
+			logger.Error("Invalid LCCN directory: %q", info.Name())
+			continue
+		}
+
+		lccnDirs = append(lccnDirs, filepath.Join(path, info.Name()))
+	}
+
+	return lccnDirs
+}
+
+func makeScannedDBIssuesFromLCCNDir(path string) []*db.Issue {
+	var infos, err = fileutil.ReaddirSorted(path)
+	if err != nil {
+		logger.Error("Unable to read directory %q: %s", path, err)
+		return nil
+	}
+
+	var dbIssues []*db.Issue
+	for _, info := range infos {
+		if !info.IsDir() {
+			// We don't abort, but this situation really shouldn't be happening
+			logger.Error("Unexpected file found in LCCN dir %q while scanning for issues", info.Name())
+			continue
+		}
+
+		var issueDir = filepath.Join(path, info.Name())
+		var dbIssue, err = db.NewIssueFromScanDir(issueDir)
+		if err != nil {
+			logger.Error("Unable to make DB Issue for %q: %s", issueDir, err)
+			continue
+		}
+
+		dbIssues = append(dbIssues, dbIssue)
+	}
+
+	return dbIssues
+}
+
+// validScanFiles ensures the PDF and TIFF files match
+func validScanFiles(path string) bool {
+	logger.Warn("Not implemented")
+	return false
+}
+
+func validScanPDFDPI(path string) bool {
+	logger.Warn("Not implemented")
+	return false
 }
