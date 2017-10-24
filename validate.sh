de77eadbb4cbb63c1b19578b7a78507d451e841c
Jeremy Echols <jechols@uoregon.edu>
2017-10-24 08:53:07 -0700

    Print validate.sh output to stderr

--- validate.sh
+++ validate.sh
@@ -4,6 +4,10 @@
 # unnoticed errors
 set -eu
 
+err() {
+  echo $@ >&2
+}
+
 IFS=''
 unformatted=$(find src/ -name "*.go" | xargs gofmt -l -s)
 linter=$(golint src/...)
@@ -12,33 +16,33 @@ vet=$(go vet -printfuncs Debugf,Infof,Warnf,Errorf,Criticalf,Fatalf ./src/...  2
 result=0
 
 if [[ $unformatted != "" ]]; then
-  echo "gofmt reports issues:"
-  echo "---------------------"
-  echo $unformatted
+  err "gofmt reports issues:"
+  err "---------------------"
+  err $unformatted
   result=1
 fi
 
 if [[ $linter != "" ]]; then
   if [[ $result == 1 ]]; then
-    echo
+    err
   fi
-  echo "linter reports issues:"
-  echo "----------------------"
-  echo $linter
+  err "linter reports issues:"
+  err "----------------------"
+  err $linter
   result=1
 fi
 
 if [[ $vet != "" ]]; then
   if [[ $result == 1 ]]; then
-    echo
+    err
   fi
-  echo "go vet reports issues:"
-  echo "----------------------"
-  echo $vet
+  err "go vet reports issues:"
+  err "----------------------"
+  err $vet
   result=1
 fi
 
 if [[ $result == 1 ]]; then
-  echo
+  err
 fi
 exit $result
