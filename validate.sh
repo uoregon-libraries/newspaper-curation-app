c7d88c54b981b1aac66fdff9f49b4b2962a29446
Jeremy Echols <jechols@uoregon.edu>
2017-10-24 07:23:29 -0700

    Validate code before compilation

---
+++ validate.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+#
+# validate.sh runs various code analysis tools to reduce the likelihood of
+# unnoticed errors
+set -eu
+
+IFS=''
+unformatted=$(find src/ -name "*.go" | xargs gofmt -l -s)
+linter=$(golint src/...)
+vet=$(go vet ./src/...  2>&1 || true)
+
+result=0
+
+if [[ $unformatted != "" ]]; then
+  echo "gofmt reports issues:"
+  echo "---------------------"
+  echo $unformatted
+  result=1
+fi
+
+if [[ $linter != "" ]]; then
+  if [[ $result == 1 ]]; then
+    echo
+  fi
+  echo "linter reports issues:"
+  echo "----------------------"
+  echo $linter
+  result=1
+fi
+
+if [[ $vet != "" ]]; then
+  if [[ $result == 1 ]]; then
+    echo
+  fi
+  echo "go vet reports issues:"
+  echo "----------------------"
+  echo $vet
+  result=1
+fi
+
+if [[ $result == 1 ]]; then
+  echo
+fi
+exit $result
