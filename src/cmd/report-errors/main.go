bf61e55af3b3e2df590a409bcb72ab2bb6848b4c
Jeremy Echols <jechols@uoregon.edu>
2018-01-31 11:59:04 -0800

    Use cli package for report-errors

--- src/cmd/report-errors/main.go
+++ src/cmd/report-errors/main.go
@@ -3,55 +3,14 @@
 package main
 
 import (
-	"config"
-	"fmt"
+	"cli"
 	"issuewatcher"
 
-	"os"
-
-	"github.com/jessevdk/go-flags"
 	"github.com/uoregon-libraries/gopkg/logger"
-	"github.com/uoregon-libraries/gopkg/wordutils"
 )
 
-// Command-line options
-var opts struct {
-	ConfigFile string `short:"c" long:"config" description:"path to Black Mamba config file" required:"true"`
-}
-
-var p *flags.Parser
-var conf *config.Config
-
-// wrap is a helper to wrap a usage message at 80 characters and print a
-// newline afterward
-func wrap(msg string) {
-	fmt.Fprint(os.Stderr, wordutils.Wrap(msg, 80))
-	fmt.Fprintln(os.Stderr)
-}
-
-func usageFail(format string, args ...interface{}) {
-	wrap(fmt.Sprintf(format, args...))
-	fmt.Fprintln(os.Stderr)
-	p.WriteHelp(os.Stderr)
-	os.Exit(1)
-}
-
-func getOpts() {
-	p = flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash)
-	var _, err = p.Parse()
-
-	if err != nil {
-		usageFail("Error: %s", err)
-	}
-
-	conf, err = config.Parse(opts.ConfigFile)
-	if err != nil {
-		logger.Fatalf("Config error: %s", err)
-	}
-}
-
 func main() {
-	getOpts()
+	var conf = cli.Simple().GetConf()
 	var watcher = issuewatcher.New(conf)
 	var err = watcher.Deserialize()
 	if err != nil {
