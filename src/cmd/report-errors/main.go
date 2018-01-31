0076c104eb6621c6b44f851e4cb31b109f674d16
Jeremy Echols <jechols@uoregon.edu>
2018-01-31 10:56:31 -0800

    Copy old report-errors code

---
+++ src/cmd/report-errors/main.go
@@ -0,0 +1,64 @@
+// This app reads the finder cache to report all known errors
+
+package main
+
+import (
+	"fmt"
+	"issuefinder"
+
+	"os"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/uoregon-libraries/gopkg/fileutil"
+	"github.com/uoregon-libraries/gopkg/logger"
+	"github.com/uoregon-libraries/gopkg/wordutils"
+)
+
+// Command-line options
+var opts struct {
+	CacheFile string `long:"cache-file" description:"Path to the finder cache" required:"true"`
+}
+
+var p *flags.Parser
+
+// wrap is a helper to wrap a usage message at 80 characters and print a
+// newline afterward
+func wrap(msg string) {
+	fmt.Fprint(os.Stderr, wordutils.Wrap(msg, 80))
+	fmt.Fprintln(os.Stderr)
+}
+
+func usageFail(format string, args ...interface{}) {
+	wrap(fmt.Sprintf(format, args...))
+	fmt.Fprintln(os.Stderr)
+	p.WriteHelp(os.Stderr)
+	os.Exit(1)
+}
+
+func getOpts() {
+	p = flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash)
+	var _, err = p.Parse()
+
+	if err != nil {
+		usageFail("Error: %s", err)
+	}
+
+	if !fileutil.IsFile(opts.CacheFile) {
+		usageFail("ERROR: --cache-file %#v is not a valid file", opts.CacheFile)
+	}
+}
+
+func main() {
+	getOpts()
+	var finder, err = issuefinder.Deserialize(opts.CacheFile)
+	if err != nil {
+		logger.Fatalf("Unable to deserialize the cache file %#v: %s", opts.CacheFile, err)
+	}
+
+	finder.Errors.Index()
+
+	// Report all errors
+	for _, e := range finder.Errors.Errors {
+		logger.Errorf(e.Message())
+	}
+}
