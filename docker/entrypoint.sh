21b4f3c743b81ae726763e3fe5bf0547adb41c52
Jeremy Echols <jechols@uoregon.edu>
2018-02-06 09:05:42 -0800

    Add better output when env vars are missing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -eu
+
+if [[ ${APP_URL:-} == "" ]]; then
+  echo "APP_URL environment variable is not set; if the application won't"
+  echo "start due to an invalid WEBROOT, this could be why"
+  echo
+fi
+if [[ ${BM_NEWS_WEBROOT:-} == "" ]]; then
+  echo "BM_NEWS_WEBROOT must be set"
+  exit 1
+fi
+
 echo "Waiting for database connectivity"
 wait_for_database
 
