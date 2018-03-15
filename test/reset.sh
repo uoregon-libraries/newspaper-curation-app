f76a916429bd2466bdb1caf09cfd5028e4392c6e
Jeremy Echols <jechols@uoregon.edu>
2018-03-15 13:06:48 -0700

    test: Make reset.sh really reset the app state
    Deletes all jobs, issues, job logs, and batches from the database, and
    restarts the entire app stack

--- test/reset.sh
+++ test/reset.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+docker-compose exec db mysql -unca -pnca -Dnca -e "delete from jobs; delete from issues; delete from job_logs; delete from batches;"
+docker-compose down
 ./makemine.sh
 rm fakemount/* -rf
 go run copy-sources.go .
+./make-older.sh
+docker-compose up -d
