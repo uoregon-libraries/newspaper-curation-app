fcb7707056b67698f13ebf9009374e2ebf18f381
Jeremy Echols <jechols@uoregon.edu>
2018-04-02 11:22:04 -0700

    test: Make reset.sh use the docker root directory
    When running `docker-compose` commands in a subdir, it seems to
    sometimes work, but some things don't work quite right - the .env file,
    for instance.  This should fix those edge cases.

--- test/reset.sh
+++ test/reset.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+pushd .
+cd ..
 docker-compose exec db mysql -unca -pnca -Dnca -e "delete from jobs; delete from issues; delete from job_logs; delete from batches;"
 docker-compose down
+
+popd
 ./makemine.sh
 rm fakemount/* -rf
 go run copy-sources.go .
 ./make-older.sh
+
+pushd .
+cd ..
 docker-compose up -d
+popd
