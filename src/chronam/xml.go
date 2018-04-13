dc56dbfac92605f1bc809b34af8aa6d002666d25
Jeremy Echols <jechols@uoregon.edu>
2018-04-13 12:36:17 -0700

    chronam: Simplify ParseBatchXML
    Takes a raw file instead of a batch directory, so it can be used on
    legacy batches as well as new ones

--- src/chronam/xml.go
+++ src/chronam/xml.go
@@ -2,10 +2,8 @@ package chronam
 
 import (
 	"encoding/xml"
-
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
 
 	"github.com/uoregon-libraries/gopkg/fileutil"
 )
@@ -24,24 +22,23 @@ type BatchIssueXML struct {
 	Content      string `xml:",innerxml"`
 }
 
-// ParseBatchXML finds the batch.xml file for a given batch path and returns
-// the decoded XML structure.  This is intended primarily for retrieving the
-// list of issues in a batch.
-func ParseBatchXML(batchDir string) (*BatchXML, error) {
-	var xmlFile = filepath.Join(batchDir, "data", "batch.xml")
+// ParseBatchXML opens the given batch.xml file for a given batch path and
+// returns the decoded XML structure.  This is intended primarily for
+// retrieving the list of issues in a batch.
+func ParseBatchXML(xmlFile string) (*BatchXML, error) {
 	if !fileutil.IsFile(xmlFile) {
-		return nil, fmt.Errorf("batch directory %#v has no batch.xml", batchDir)
+		return nil, fmt.Errorf("%q is not a file", xmlFile)
 	}
 
 	var contents, err = ioutil.ReadFile(xmlFile)
 	if err != nil {
-		return nil, fmt.Errorf("batch XML file (%#v) can't be read: %s", xmlFile, err)
+		return nil, fmt.Errorf("batch XML file (%q) can't be read: %s", xmlFile, err)
 	}
 
 	var bx BatchXML
 	err = xml.Unmarshal(contents, &bx)
 	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal batch XML %#v: %s", xmlFile, err)
+		return nil, fmt.Errorf("unable to unmarshal batch XML %q: %s", xmlFile, err)
 	}
 
 	return &bx, nil
