dc4a999b600bda1b9404dd77231f7fe849198682
Jeremy Echols <jechols@uoregon.edu>
2018-04-30 08:37:07 -0700

    deploy: Don't destroy vendored dependencies
    This seems unnecessary (and is easy enough to do manually) and
    potentially risky if a dep is unreachable and a deploy is needed ASAP
    for some emergency fix or something

--- rhel7/deploy.sh
+++ rhel7/deploy.sh
@@ -40,7 +40,7 @@ fi
 cp src/version/version.go /tmp/old-version.go
 sed -i "s|\"$|$version\"|" src/version/version.go
 
-make cleanall
+make clean
 make
 
 cp /tmp/old-version.go src/version/version.go
