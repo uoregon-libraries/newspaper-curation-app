9eac0d00626d4a9d62408f5a1325e987d5e1dac8
Jeremy Echols <jechols@uoregon.edu>
2018-05-18 08:25:53 -0700

    move-error-issues: Finish the implementation
    Kills the very simple "print out commands" prototype and fleshes out the
    full copy-verify-remove process.  Also updates the database on success,
    and logs errors very carefully.

--- src/cmd/move-errored-issues/main.go
+++ src/cmd/move-errored-issues/main.go
@@ -5,6 +5,9 @@ import (
 	"config"
 	"db"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/uoregon-libraries/gopkg/fileutil"
 	"github.com/uoregon-libraries/gopkg/logger"
@@ -46,6 +49,64 @@ func main() {
 	}
 
 	for _, issue := range issues {
-		fmt.Printf("mv %s %s/%s", issue.Location, opts.Dest, issue.HumanName)
+		var finalDest = filepath.Join(opts.Dest, issue.HumanName)
+		err = fileutil.CopyDirectory(issue.Location, finalDest)
+		// We halt on any error because this can mean things like filesystem
+		// problems / network disk disconnects, etc.  Better to have to rerun the
+		// script than wade through dozens of errors to determine if any are
+		// different.
+		if err != nil {
+			logger.Errorf("Unable to copy issue from %q to %q: %s", issue.Location, finalDest, err)
+			break
+		}
+
+		// Drop a file into the copied directory with the error notes
+		var errFile = filepath.Join(finalDest, "error.txt")
+		err = ioutil.WriteFile(errFile, []byte(issue.Error), 0660)
+
+		// I'm actually not sure what do to here - the copy succeeded, but the
+		// creation of a small text file failed.  This hopefully never happens.
+		if err != nil {
+			logger.Errorf("Unable to create error.txt file %q: %s", errFile, err)
+			err = os.RemoveAll(finalDest)
+			// If we couldn't create the error.txt file, there's reason to be
+			// concerned that we can't delete the destination folder, in which case
+			// garbage gets left over, so we have to log loudly again....  Man,
+			// filesystem-based workflows are riddled with failure potential.
+			if err != nil {
+				logger.Errorf("Unable to remove copied issue in %q: %s (you must remove this manually!", finalDest, err)
+			}
+
+			// In all cases, gtfo because something messed up is going on
+			break
+		}
+
+		// Fry the source, because all possible error situations in the copy were avoided
+		err = os.RemoveAll(issue.Location)
+
+		// Things are a lot trickier if we have an error on removal - the issue was
+		// copied successfully, so failing to remove it pretty much means we print
+		// a loud error stating the source has to be removed manually.  But we
+		// don't halt the operation, because things are effectively "good".
+		if err != nil {
+			logger.Errorf("Unable to remove issue in %q: %s (you must remove this manually!)", issue.Location, err)
+		}
+
+		// Update the issue so we never deal with it again (but we preserve it so
+		// we can refer to it again if necessary)
+		issue.Location = ""
+		issue.Ignored = true
+		err = issue.Save()
+
+		// If we couldn't save to the database, we're very unhappy.  This is
+		// possibly the worst scenario, because it's not all that unlikely compared
+		// to other post-copy failures.  The data has been copied and the source
+		// removed, so we can't really back out... but we also can't update the
+		// issue in the database.  Response?  Somebody has to manually fix the
+		// database :-/
+		if err != nil {
+			logger.Errorf("Unable to update issue %d in the database: %s (you must fix this manually!)", issue.ID, err)
+			break
+		}
 	}
 }
