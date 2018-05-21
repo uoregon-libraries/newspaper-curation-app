f06d773184d9f658a087404514f9705d098035d2
Jeremy Echols <jechols@uoregon.edu>
2018-05-21 11:24:54 -0700

    test: Fix backup and restore scripts
    - Wraps commands in sudo
    - Centralizes docker-compose up and down
    - Moves to root project dir before running compose commands
    - Stops and starts the whole stack, not just db, so that when db is
      restarted the rest of the stack isn't confused

--- test/restore-state.sh
+++ test/restore-state.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -eu
 
-docker-compose stop db
-docker-compose rm -f db
-
-cp ./nca.tgz /var/lib/docker/volumes/nca_db/_data
-pushd .
-cd /var/lib/docker/volumes/nca_db/_data
-rm nca -rf
-tar -xzf ./nca.tgz
-popd
-
-rm fakemount -rf
-tar -xf ./fakemount.tar
-
-docker-compose up -d db
+sudo su -c "source ./_backrest.lib.sh && dorestore"
