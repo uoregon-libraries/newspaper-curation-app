ba79381983c6bb5cf4df62bbf407c3559cb7044a
Jeremy Echols <jechols@uoregon.edu>
2018-05-21 11:26:16 -0700

    test: Remove finder.cache on reset

--- test/reset.sh
+++ test/reset.sh
@@ -3,7 +3,14 @@ set -eu
 
 pushd .
 cd ..
+
+# Fry the issues, jobs, and batches from the database
 docker-compose exec db mysql -unca -pnca -Dnca -e "delete from jobs; delete from issues; delete from job_logs; delete from batches;"
+
+# Remove the finder cache, but *not* the cached web data - this gives us a
+# quicker upstart since we'll only have to rescan the filesystem
+docker-compose exec workers rm /var/local/news/nca/cache/finder.cache
+
 docker-compose down
 
 popd
