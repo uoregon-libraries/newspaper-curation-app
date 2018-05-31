e44a3f3ccbdb1022bf28463e92e9a51a513d0f64
Jeremy Echols <jechols@uoregon.edu>
2018-05-31 15:52:44 -0700

    test: Make reset.sh more resilient
    - Makes sure DB container is up and ready before trying to send query
    - Runs cache deletion in a transient container instead of expecting
      workers services to be up and running

--- test/reset.sh
+++ test/reset.sh
@@ -5,13 +5,15 @@ pushd .
 cd ..
 
 # Fry the issues, jobs, and batches from the database
+docker-compose up -d db
+docker-compose run --rm workers wait_for_database
 docker-compose exec db mysql -unca -pnca -Dnca -e "delete from jobs; delete from issues; delete from job_logs; delete from batches;"
+
+docker-compose down
 
 # Remove the finder cache, but *not* the cached web data - this gives us a
 # quicker upstart since we'll only have to rescan the filesystem
-docker-compose exec workers rm /var/local/news/nca/cache/finder.cache
-
-docker-compose down
+docker-compose run --rm workers rm /var/local/news/nca/cache/finder.cache
 
 popd
 ./makemine.sh
