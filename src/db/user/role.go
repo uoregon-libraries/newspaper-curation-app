afaa13bc93de0e1c3fc831e87b1d8948d80e38de
Jeremy Echols <jechols@uoregon.edu>
2018-06-05 08:27:17 -0700

    db/user: Expose more information about Roles

--- src/db/user/role.go
+++ src/db/user/role.go
@@ -1,28 +1,49 @@
 package user
 
+import "regexp"
+
 // A Role defines a grouping of privileges
 type Role struct {
 	Name string
+	Desc string
+}
+
+var oneLineRegexp = regexp.MustCompile(`\s*\n\s*`)
+
+// oneline turns a multi-line string into a single line by collapsing newlines
+// surrounded by any amount of whitespace (space, tab, etc.) into a single
+// ASCII space.
+func oneline(s string) string {
+	return oneLineRegexp.ReplaceAllString(s, " ")
 }
 
 // Hard-coded list of roles
 var (
-	RoleAny             = newRole("-any-")
-	RoleAdmin           = newRole("admin")
-	RoleTitleManager    = newRole("title manager")
-	RoleIssueCurator    = newRole("issue curator")
-	RoleIssueReviewer   = newRole("issue reviewer")
-	RoleUserManager     = newRole("user manager")
-	RoleMOCManager      = newRole("marc org code manager")
-	RoleWorkflowManager = newRole("workflow manager")
+	RoleAny   = newRole("-any-", "N/A")
+	RoleAdmin = newRole("admin",
+		`No restrictions.  These users can modify data not meant for modification
+		outside of initial setup and data repair situations, such as sftp
+		user/password, LCCNs which have already been validated, etc.`)
+	RoleTitleManager = newRole("title manager",
+		`Has access to add and change newspaper titles, including the ability to
+		view the sftp authorization information`)
+	RoleIssueCurator = newRole("issue curator",
+		`Can modify issue metadata and push issues to the review queue`)
+	RoleIssueReviewer = newRole("issue reviewer",
+		`Can modify issue metadata, push issues to the review queue, and mark issues as reviewed`)
+	RoleUserManager = newRole("user manager",
+		`Can add, edit, and delete users.  User managers can assign any rights to
+		others which have been assigned to them.`)
+	RoleMOCManager      = newRole("marc org code manager", "Has access to add new MARC Org Codes")
+	RoleWorkflowManager = newRole("workflow manager", "Can queue SFTP and scanned issues for processing")
 )
 
 // roles is our internal map of string to Role object
 var roles = make(map[string]*Role)
 
 // newRole is internal as the list of roles shouldn't be modified by anything external
-func newRole(name string) *Role {
-	var r = &Role{name}
+func newRole(name, desc string) *Role {
+	var r = &Role{Name: name, Desc: oneline(desc)}
 	roles[name] = r
 	return r
 }
