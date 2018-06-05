40f913cb6441db528ffc2541cbfce0d2244e1f68
Jeremy Echols <jechols@uoregon.edu>
2018-06-05 11:47:41 -0700

    Remove meaningless APP_URL warning

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-if [[ ${APP_URL:-} == "" ]]; then
-  echo "APP_URL environment variable is not set; if the application won't"
-  echo "start due to an invalid WEBROOT, this could be why"
-  echo
-fi
 if [[ ${NCA_NEWS_WEBROOT:-} == "" ]]; then
   echo "NCA_NEWS_WEBROOT must be set"
   exit 1
