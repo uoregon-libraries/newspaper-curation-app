7b79214af8eb31e0641cd774c888537479ef2d9e
Jeremy Echols <jechols@uoregon.edu>
2019-03-21 08:40:33 -0700

    upload-server: centralize response functionality
    Adds new responder type to wrap the server as well as request data.
    Reduces the passing around of (w, req) everywhere so a given function's
    purpose is a bit clearer.  Also will help as we add more
    request-specific data like sessions.

--- src/cmd/upload-server/handlers.go
+++ src/cmd/upload-server/handlers.go
@@ -2,70 +2,56 @@ package main
 
 import (
 	"net/http"
-	"path"
 
 	"github.com/uoregon-libraries/newspaper-curation-app/src/db"
 )
 
-func (s *srv) notFoundHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		s.middleware.Log(w, req, http.HandlerFunc(http.NotFound), s.logger.Debugf, "Unrouted request")
-	}
+func (s *srv) notFoundHandler() *responder {
+	return s.respond(func(r *responder) {
+		s.middleware.Log(r.w, r.req, http.HandlerFunc(http.NotFound), s.logger.Debugf, "Unrouted request")
+	})
 }
 
-func (s *srv) redirectSubpath(w http.ResponseWriter, req *http.Request, subpath string, code int) {
-	http.Redirect(w, req, path.Join(s.webroot.Path, subpath), code)
+func (s *srv) redirectSubpathHandler(subpath string, code int) *responder {
+	return s.respond(func(r *responder) {
+		r.redirectSubpath(subpath, code)
+	})
 }
 
-func (s *srv) redirectSubpathHandler(subpath string, code int) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		s.redirectSubpath(w, req, subpath, code)
-	}
-}
-
-func (s *srv) loginFormHandler() http.HandlerFunc {
+func (s *srv) loginFormHandler() http.Handler {
 	var t = s.layout.MustBuild("login.go.html")
-	return func(w http.ResponseWriter, req *http.Request) {
-		s.render(w, req, t, nil)
-	}
+	return s.respond(func(r *responder) {
+		r.render(t, nil)
+	})
 }
 
-func (s *srv) loginSubmitHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
+func (s *srv) loginSubmitHandler() http.Handler {
+	return s.respond(func(r *responder) {
 		var t *db.Title
 		var err error
 
-		var name, pass = req.FormValue("loginname"), req.FormValue("password")
-		if s.debug {
+		var name, pass = r.req.FormValue("loginname"), r.req.FormValue("password")
+		if r.server.debug {
 			// Let's make sure it's *really* hard to leave debug on by accident
-			s.logger.Warnf("Debug mode: not validating password for %q", name)
+			r.server.logger.Warnf("Debug mode: not validating password for %q", name)
 			t, err = db.FindTitle("sftp_user = ?", name)
 		} else {
 			t, err = db.FindTitle("sftp_user = ? AND sftp_pass = ?", name, pass)
 		}
 		if err != nil {
-			s.logger.Errorf("Unable to query database for user and password: %s", err)
-			s.internalServerError(w, req)
+			r.server.logger.Errorf("Unable to query database for user and password: %s", err)
+			r.internalServerError()
 			return
 		}
 
 		if t.ID == 0 {
-			s.logger.Infof("Invalid login attempt for %q", name)
-			s.redirectSubpath(w, req, "login", http.StatusSeeOther)
+			r.server.logger.Infof("Invalid login attempt for %q", name)
+			r.redirectSubpath("login", http.StatusSeeOther)
 			return
 		}
 
 		// TODO: Store user info in session
-		s.logger.Infof("%q authenticated for title %#v", name, t)
-		s.redirectSubpath(w, req, "upload", http.StatusSeeOther)
-	}
-}
-
-func (s *srv) error(w http.ResponseWriter, req *http.Request, msg string, code int) {
-	w.WriteHeader(code)
-	s.empty.Execute(w, map[string]string{"Alert": msg})
-}
-
-func (s *srv) internalServerError(w http.ResponseWriter, req *http.Request) {
-	s.error(w, req, "Internal server error.  Please try again, and contact support if the problem persists", 500)
+		r.server.logger.Infof("%q authenticated for title %#v", name, t)
+		r.redirectSubpath("upload", http.StatusSeeOther)
+	})
 }
---
+++ src/cmd/upload-server/responder.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"path"
+
+	"github.com/uoregon-libraries/gopkg/tmpl"
+)
+
+// responder wraps the server, request, and response writer to simplify common
+// operations which need this data.  A responder will automatically render our
+// 500 page if any errors occur, bypassing other application logic.
+type responder struct {
+	w       http.ResponseWriter
+	req     *http.Request
+	err     error
+	server  *srv
+	handler func(r *responder)
+}
+
+// respond returns a responder for handling HTTP requests
+func (s *srv) respond(handler func(r *responder)) *responder {
+	return &responder{server: s, handler: handler}
+}
+
+// ServeHTTP implements http.Handler so a responder can act as an arbitrary
+// request handler
+func (r *responder) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.w = w
+	r.req = req
+	r.handler(r)
+}
+
+func (r *responder) redirectSubpath(subpath string, code int) {
+	http.Redirect(r.w, r.req, path.Join(r.server.webroot.Path, subpath), code)
+}
+
+func (r *responder) error(msg string, code int) {
+	r.w.WriteHeader(code)
+	r.server.empty.Execute(r.w, map[string]string{"Alert": msg})
+}
+
+func (r *responder) internalServerError() {
+	r.error("Internal server error.  Please try again, and contact support if the problem persists", 500)
+}
+
+func (r *responder) render(t *tmpl.Template, data map[string]interface{}) {
+	var b = new(bytes.Buffer)
+	var err = t.Execute(b, data)
+	if err != nil {
+		r.server.logger.Errorf("Unable to render template %s: %s", t.Name, err)
+		r.internalServerError()
+		return
+	}
+
+	b.WriteTo(r.w)
+}
