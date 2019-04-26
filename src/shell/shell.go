2e335e24ecaf9c6e1560e8e852c2802274606b8d
lsat12357 <lsato@uoregon.edu>
2019-04-26 09:53:15 -0700

    shell: use passed-in Logger instead of context

--- src/shell/shell.go
+++ src/shell/shell.go
@@ -11,7 +11,7 @@ import (
 	"github.com/uoregon-libraries/gopkg/logger"
 )
 
-func _exec(cmd *exec.Cmd, binary string, args ...string) (ok bool) {
+func _exec(cmd *exec.Cmd, binary string, jobLogger Logger, args ...string) (ok bool) {
 	logger.Debugf(`Running "%s %s"`, binary, strings.Replace(strings.Join(args, " "), "%", "%%", -1))
 	var output, err = cmd.CombinedOutput()
 	if err != nil {
@@ -19,6 +19,7 @@ func _exec(cmd *exec.Cmd, binary string, args ...string) (ok bool) {
 		for _, line := range bytes.Split(output, []byte("\n")) {
 			logger.Debugf("--> %s", line)
 		}
+		jobLogger.Log.Errorf(`Failed to run "%s %s": %s`, binary, strings.Join(args, " "), err)
 
 		return false
 	}
@@ -28,23 +29,15 @@ func _exec(cmd *exec.Cmd, binary string, args ...string) (ok bool) {
 
 // Exec attempts to run the given command, using logger to give consistent
 // formatting to whatever the command spits out if an error occurs
-func Exec(binary string, args ...string) (ok bool) {
+func Exec(binary string, jobLogger Logger, args ...string) (ok bool) {
 	var cmd = exec.Command(binary, args...)
-	return _exec(cmd, binary, args...)
+	return _exec(cmd, binary, jobLogger, args...)
 }
 
 // ExecSubgroup is just like Exec, but sets the process to run in its own group
 // so it doesn't get killed on CTRL+C
-func ExecSubgroup(binary string, args ...string) (ok bool) {
+func ExecSubgroup(binary string, jobLogger Logger, args ...string) (ok bool) {
 	var cmd = exec.Command(binary, args...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	return _exec(cmd, binary, args...)
-}
-
-// ExecSubgroupWithContext is just like ExecSubgroup, but with a string context
-func ExecSubgroupWithContext(binary string, context string, args ...string) (ok bool) {
-	var cmd = exec.Command(binary, args...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	args = append(args, context)
-	return _exec(cmd, binary, args...)
+	return _exec(cmd, binary, jobLogger, args...)
 }
