c9456a7d8a24fb400b50af7ee519a18010bfa134
Jeremy Echols <jechols@uoregon.edu>
2020-11-24 07:19:33 -0800

    Add scripts for easier local dev

---
+++ scripts/localdev.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+wait_for_database() {
+  MAX_TRIES=30
+  TRIES=0
+  while true; do
+    mysql -unca -h127.0.0.1 -pnca -e 'ALTER DATABASE nca charset=utf8'
+    st=$?
+    if [[ $st == 0 ]]; then
+      return 0
+    fi
+
+    let TRIES++
+    if [[ $TRIES == $MAX_TRIES ]]; then
+      echo "ERROR: Unable to connect to the database after $MAX_TRIES attempts"
+      return 2
+    fi
+
+    sleep 5
+  done
+}
+
+# Resets the database, deleting and rebuilding all the seed data
+resetdb() {
+  docker-compose down -v
+  docker-compose up -d db iiif
+  wait_for_database && migrate && load_seed_data
+}
+
+_get_bulk_lccns() {
+  ./bin/bulk-issue-queue -c ./settings --type=$1 | awk '{print $1}' | grep -v "^\(Valid\|---\)"
+}
+
+# Runs the database reset, then resets test issues in our fake mount area
+prep_for_testing() {
+  sudo echo -n
+  resetdb
+  resetfakemount
+  bulk_queue_borndigital
+
+  # Reset the IIIF server since it'll be looking at a mount point we just deleted
+  docker-compose stop iiif
+  docker-compose rm -f iiif
+  docker-compose up -d iiif
+}
+
+# Sets up all fake uploads from the test/fakemount dir, then bulk-queues all
+# issues without errors
+resetfakemount() {
+  pushd .
+  cd ./test
+  ./makemine.sh
+  rm fakemount/* -rf
+  go run copy-sources.go .
+  ./make-older.sh
+  popd
+}
+
+bulk_queue_borndigital() {
+  # Bulk-queue all issues we can
+  make bin/bulk-issue-queue || return 1
+  for lccn in $(_get_bulk_lccns borndigital); do
+    ./bin/bulk-issue-queue -c ./settings --type=borndigital --key=$lccn
+  done
+}
+
+migrate() {
+  goose -dir ./db/migrations/ mysql 'nca:nca@tcp(localhost:3306)/nca' up
+}
+
+load_seed_data() {
+  mysql -unca -pnca -Dnca -h127.0.0.1 < ./docker/mysql/nca-seed-data.sql
+}
+
+upload_server() {
+  make bin/upload-server || return 1
+  export NCA_DBCONNECT="nca:nca@tcp(localhost:3306)/nca"
+  export NCA_SECRET="shhhh"
+  ./bin/upload-server --debug --bind-address ":8080" --webroot "http://localhost:8080"
+}
+
+server() {
+  make bin/server || return 1
+  ./bin/server -c ./settings --debug
+}
+
+workers() {
+  make bin/run-jobs || return 1
+  ./bin/run-jobs -c ./settings watchall
+}
