1dd46550b3ca9b7bfe98bafb6921a5734c2c5d3d
Jeremy Echols <jechols@uoregon.edu>
2021-07-16 12:01:53 -0700

    purge-dead-issues: make the command all or nothing
    Instead of one transaction per issue, it's one transaction for the
    entire operation.  Having partial success in this situation is
    *probably* fine, but somehow rubs me the wrong way.

--- src/cmd/purge-dead-issues/main.go
+++ src/cmd/purge-dead-issues/main.go
@@ -52,9 +52,36 @@ func main() {
 		logger.Fatalf("Unable to scan database for issues awaiting processing: %s", err)
 	}
 
-	for _, i := range issues {
+	// Set up a transaction so all jobs and issues are processed or skipped as
+	// one operation
+	var op = dbi.DB.Operation()
+	op.Dbg = dbi.Debug
+	op.BeginTransaction()
+	defer op.EndTransaction()
+
+	err = purge(op, issues)
+
+	if err != nil {
+		logger.Warnf("One or more errors were encountered: aborting transaction")
+		op.Rollback()
+	}
+
+	if !opts.Live {
+		logger.Warnf("DRY RUN: aborting transaction")
+		op.Rollback()
+	}
+
+	op.EndTransaction()
+	if op.Err() != nil {
+		logger.Errorf("Failed finalizing purge-jobs' transaction: %s", err)
+	}
+	logger.Debugf("Process complete")
+}
+
+func purge(dbop *magicsql.Operation, list []*models.Issue) error {
+	for _, i := range list {
 		logger.Debugf("Examining issue id %d (%s)", i.ID, i.HumanName)
-		err = purgeIssue(i)
+		var err = purgeIssue(dbop, i)
 		if err == nil {
 			logger.Infof("Issue id %d (%s): purged", i.ID, i.HumanName)
 			continue
@@ -62,15 +89,16 @@ func main() {
 
 		if errors.As(err, &fatalError{}) {
 			logger.Warnf("Issue id %d (%s): fatal error checking purgability: %s", i.ID, i.HumanName, err)
-		} else {
-			logger.Infof("Issue id %d (%s): skipping: %s", i.ID, i.HumanName, err)
+			return err
 		}
+
+		logger.Infof("Issue id %d (%s): skipping: %s", i.ID, i.HumanName, err)
 	}
 
-	logger.Debugf("Process complete")
+	return nil
 }
 
-func purgeIssue(i *models.Issue) error {
+func purgeIssue(dbop *magicsql.Operation, i *models.Issue) error {
 	if i.BatchID != 0 {
 		return fmt.Errorf("issue is tied to a batch")
 	}
@@ -115,7 +143,7 @@ func purgeIssue(i *models.Issue) error {
 
 	// Getting here means no errors, so we can kick off the purge.  All errors
 	// from doPurge are fatal.
-	err = doPurge(i, purgeJobs)
+	err = doPurge(dbop, i, purgeJobs)
 	if err != nil {
 		err = fatalError{err}
 	}
@@ -125,39 +153,25 @@ func purgeIssue(i *models.Issue) error {
 
 // doPurge finalizes the dead/on-hold jobs' statuses and then kicks off a new
 // job to remove the issue using the existing QueueRemoveErroredIssue task.
-func doPurge(issue *models.Issue, purgeJobs []*models.Job) error {
-	// Set up a transaction so all jobs and the issue are processed or skipped as
-	// one operation
-	var op = dbi.DB.Operation()
-	op.Dbg = dbi.Debug
-	op.BeginTransaction()
-	defer op.EndTransaction()
-
+func doPurge(dbop *magicsql.Operation, issue *models.Issue, purgeJobs []*models.Job) error {
+	var purgeReason = "Issue failed getting through workflow:\n"
 	for _, j := range purgeJobs {
 		logger.Debugf("Canceling job id %d (%s)", j.ID, j.Type)
-		var err = cancelJob(op, j)
+		if j.Status == string(models.JobStatusFailed) {
+			purgeReason += fmt.Sprintf("- Job %d (%s) failed too many times\n", j.ID, j.Type)
+		}
+
+		var err = cancelJob(dbop, j)
 		if err != nil {
-			op.Rollback()
 			return err
 		}
 	}
 
+	issue.Save(models.ActionTypeInternalProcess, models.SystemUser.ID, purgeReason)
 	var joblist = jobs.GetJobsForRemoveErroredIssue(issue, erroredIssuesPath)
-	var err = jobs.QueueSerialOp(op, joblist...)
+	var err = jobs.QueueSerialOp(dbop, joblist...)
 	if err != nil {
-		op.Rollback()
 		return fmt.Errorf("queueing jobs to purge issue %d: %s", issue.ID, err)
-	}
-
-	if !opts.Live {
-		logger.Warnf("DRY RUN: aborting transaction")
-		op.Rollback()
-		return nil
-	}
-
-	op.EndTransaction()
-	if op.Err() != nil {
-		return fmt.Errorf("finalizing purge-jobs' transaction for issue %d: %s", issue.ID, err)
 	}
 
 	return nil
@@ -165,11 +179,11 @@ func doPurge(issue *models.Issue, purgeJobs []*models.Job) error {
 
 // cancelJob deals with changing a job's status to failed_done while
 // guarding against accidental purge-jobs
-func cancelJob(op *magicsql.Operation, job *models.Job) error {
+func cancelJob(dbop *magicsql.Operation, job *models.Job) error {
 	switch models.JobStatus(job.Status) {
 	case models.JobStatusOnHold, models.JobStatusFailed:
 		job.Status = string(models.JobStatusFailedDone)
-		job.SaveOp(op)
+		job.SaveOp(dbop)
 		return nil
 
 	default:
