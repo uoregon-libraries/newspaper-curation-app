3191c4b549da291c6ff7f60df7ca9cc2dcad847a
Jeremy Echols <jechols@uoregon.edu>
2021-07-27 14:05:30 -0700

    Rename IsReadyForReview -> CanReturnToReview
    This function name just wrecked my day because I was reusing it in the
    wrong context.  Grr.

--- src/cmd/server/internal/workflowhandler/schema.go
+++ src/cmd/server/internal/workflowhandler/schema.go
@@ -214,11 +214,11 @@ func (i *Issue) Errors() []apperr.Error {
 	return i.validationErrors
 }
 
-// IsReadyForReview returns true if the issue's metadata is valid.  This is
+// CanReturnToReview returns true if the issue's metadata is valid.  This is
 // pretty specific (for now) to the process of taking an out-of-NCA issue
 // (reported as having unfixable errors) and wanting to push it straight to the
 // review queue.
-func (i *Issue) IsReadyForReview() bool {
+func (i *Issue) CanReturnToReview() bool {
 	i.ValidateMetadata()
 	return len(i.validationErrors) == 0
 }
