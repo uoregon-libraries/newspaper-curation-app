263d8849c8716e1b59b051588ef805aa575b5cb5
Jeremy Echols <jechols@uoregon.edu>
2022-07-01 14:43:00 -0700

    schema: move LastModified to its own file

--- src/schema/schema.go
+++ src/schema/schema.go
@@ -5,15 +5,12 @@ package schema
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math"
-	"os"
 	"path/filepath"
 	"regexp"
 	"sort"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/uoregon-libraries/gopkg/fileutil"
 	"github.com/uoregon-libraries/newspaper-curation-app/src/apperr"
@@ -335,37 +332,6 @@ func (i *Issue) addChildError() {
 	}
 	i.addError(apperr.New("one or more files are invalid"))
 	i.hasChildErrors = true
-}
-
-// LastModified tells us when *any* change happened in an issue's folder.  This
-// will return a meaningless value on live issues.
-func (i *Issue) LastModified() time.Time {
-	if i.WorkflowStep == WSInProduction {
-		return time.Time{}
-	}
-
-	var info, err = os.Stat(i.Location)
-	if err != nil {
-		logger.Warnf("Unable to stat %q: %s", i.Location, err)
-		return time.Now()
-	}
-	var modified = info.ModTime()
-
-	var files []os.FileInfo
-	files, err = ioutil.ReadDir(i.Location)
-	if err != nil {
-		logger.Warnf("Unable to read dir %q: %s", i.Location, err)
-		return time.Now()
-	}
-
-	for _, file := range files {
-		var mod = file.ModTime()
-		if modified.Before(mod) {
-			modified = mod
-		}
-	}
-
-	return modified
 }
 
 // METSFile returns the canonical path to an issue's METS file
