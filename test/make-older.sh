f47d9b2a504bd0721bf81a41bfc98a8aa3438767
Jeremy Echols <jechols@uoregon.edu>
2022-07-07 11:02:51 -0700

    test: fix "make-older.sh" script
    Hacks the JSON to make issue manifests similarly to how "touch -d" faked
    out NCA previously

--- test/make-older.sh
+++ test/make-older.sh
@@ -2,5 +2,8 @@
 ./makemine.sh
 daysago=${1:-4}
 if [[ -d fakemount ]]; then
-  find fakemount/ -exec touch -d "$daysago days ago" {} \;
+  for file in $(find fakemount/ -type f -name ".manifest"); do
+    cat $file | sed 's|"Created":"[^"]\+",|"Created":"1900-01-01T00:00:00.000000000-07:00",|' > $file-2
+    mv $file-2 $file
+  done
 fi
