8977fce01fedbb0ee6454bdcefad30941e088c1c
Jeremy Echols <jechols@uoregon.edu>
2022-07-08 10:50:10 -0700

    make-older.sh: re-allow custom "days ago" values

--- test/make-older.sh
+++ test/make-older.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ./makemine.sh
 daysago=${1:-4}
+dt=$(date -d "$daysago days ago" +"%Y-%m-%dT00:00:00.000000000-07:00")
+
 if [[ -d fakemount ]]; then
   for file in $(find fakemount/ -type f -name ".manifest"); do
-    cat $file | sed 's|"Created":"[^"]\+",|"Created":"1900-01-01T00:00:00.000000000-07:00",|' > $file-2
+    cat $file | sed 's|"Created":"[^"]\+",|"Created":"'$dt'",|' > $file-2
     mv $file-2 $file
   done
 fi
