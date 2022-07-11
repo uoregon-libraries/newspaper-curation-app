bfdfeeaee82018c17989ffe18792f67d586e22ef
Jeremy Echols <jechols@uoregon.edu>
2022-07-11 11:16:09 -0700

    run-jobs: use the central "last modified" logic

--- src/cmd/run-jobs/issue_pages_ready.go
+++ src/cmd/run-jobs/issue_pages_ready.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/uoregon-libraries/newspaper-curation-app/src/internal/lastmod"
 	"github.com/uoregon-libraries/newspaper-curation-app/src/internal/logger"
 )
 
@@ -18,19 +19,17 @@ var notRenamedRegex = regexp.MustCompile(`(?i:^seq-[0-9]{4}.pdf)`)
 // be moved on in the workflow.  Pages are considered ready if all of the
 // following are true:
 //
-// * The path hasn't been modified less than minAge ago
+// * The central last-modified check returns a value that's at least minAge ago
 // * No files exist that don't match the pdf regex
 //   * Dotfiles are the exception, as we simply ignore these since Bridge and Macs drop these everywhere
-// * Nothing is in the path that was modified less than minAge ago
 func pageReviewIssueReady(path string, minAge time.Duration) bool {
-	// Validate that the directory itself can be statted and hasn't been touched
-	// in at least an hour
-	var info, err = os.Stat(path)
+	var t, err = lastmod.Time(path)
 	if err != nil {
-		logger.Errorf("Unable to read %q: %s", path, err)
+		logger.Errorf("Unable to check last mod time for %q: %s", path, err)
 		return false
 	}
-	if time.Since(info.ModTime()) < minAge {
+
+	if time.Since(t) < minAge {
 		return false
 	}
 
@@ -42,6 +41,7 @@ func pageReviewIssueReady(path string, minAge time.Duration) bool {
 		return false
 	}
 
+	// Check that the filenames are all valid
 	for _, info := range infos {
 		var fName = info.Name()
 
@@ -64,12 +64,6 @@ func pageReviewIssueReady(path string, minAge time.Duration) bool {
 		// pattern are considered problems
 		if !pdfFilenameRegex.MatchString(fName) {
 			logger.Errorf("Not processing %q (%q doesn't match valid file regex)", path, fName)
-			return false
-		}
-
-		// If any file was touched less than an hour ago, we don't consider it safe
-		// to process yet
-		if time.Since(info.ModTime()) < minAge {
 			return false
 		}
 	}
