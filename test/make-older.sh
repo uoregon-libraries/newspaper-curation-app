daa8c2154b12d6f9d768e7412504361fd5ed4e65
Jeremy Echols <jechols@uoregon.edu>
2022-07-20 09:59:50 -0700

    Simplify make-older.sh

--- test/make-older.sh
+++ test/make-older.sh
@@ -5,7 +5,6 @@ dt=$(date -d "$daysago days ago" +"%Y-%m-%dT00:00:00.000000000-07:00")
 
 if [[ -d fakemount ]]; then
   for file in $(find fakemount/ -type f -name ".manifest"); do
-    cat $file | sed 's|"Created":"[^"]\+",|"Created":"'$dt'",|' > $file-2
-    mv $file-2 $file
+    sed -i 's|"Created":"[^"]\+",|"Created":"'$dt'",|' $file
   done
 fi
