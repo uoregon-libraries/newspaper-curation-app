adad77ecb859359b21c85f0837c1c29e5bdbe110
Jeremy Echols <jechols@uoregon.edu>
2023-02-09 07:20:23 -0800

    Fix vars that shadow package names

--- src/jobs/pagesplit.go
+++ src/jobs/pagesplit.go
@@ -29,7 +29,7 @@ type PageSplit struct {
 // Process combines, splits, and then renames files so they're sequential in a
 // "best guess" order.  Files are then put into place for manual processors to
 // reorder if necessary, remove duped pages, etc.
-func (ps *PageSplit) Process(config *config.Config) bool {
+func (ps *PageSplit) Process(conf *config.Config) bool {
 	ps.Logger.Debugf("Processing issue id %d (%q)", ps.DBIssue.ID, ps.Issue.Key())
 	if !ps.makeTempFiles() {
 		return false
@@ -42,8 +42,8 @@ func (ps *PageSplit) Process(config *config.Config) bool {
 		return false
 	}
 
-	ps.GhostScript = config.GhostScript
-	ps.MinPages = config.MinimumIssuePages
+	ps.GhostScript = conf.GhostScript
+	ps.MinPages = conf.MinimumIssuePages
 	return ps.process()
 }
 
