7ba283e70493b521c7f854c36b2e11393a01bf3b
Jeremy Echols <jechols@uoregon.edu>
2023-05-02 08:00:24 -0700

    derivatives/alto: pre-clean pdftotext HTML
    Fixes bugs where *really* bad data in the PDF would cause xml.Unmarshal
    to return an error

--- src/derivatives/alto/transform.go
+++ src/derivatives/alto/transform.go
@@ -5,6 +5,8 @@ import (
 	"encoding/xml"
 	"fmt"
 	"html/template"
+	"unicode"
+	"unicode/utf8"
 )
 
 // templateVars is used to inject data into the ALTO XML template
@@ -31,9 +33,21 @@ func (t *Transformer) transform() {
 
 	t.Logger.Infof("Converting pdftotext HTML to ALTO XML")
 
+	// Pre-strip any super-busted runes (control characters and invalid runes)
+	var cleaned []byte
+	var offset int
+	for offset < len(t.html) {
+		var r, w = utf8.DecodeRune(t.html[offset:])
+		offset += w
+		if r == utf8.RuneError || unicode.IsControl(r) {
+			continue
+		}
+		cleaned = utf8.AppendRune(cleaned, r)
+	}
+
 	// Parse XML to get at page attributes
 	var source Doc
-	var err = xml.Unmarshal(t.html, &source)
+	var err = xml.Unmarshal(cleaned, &source)
 	if err != nil {
 		t.err = fmt.Errorf("invalid html to unmarshal into XML: %w", err)
 		return
