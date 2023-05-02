fc52f609e84c09a63372242af49a574ed5a20863
Jeremy Echols <jechols@uoregon.edu>
2023-05-02 14:12:45 -0700

    derivatives/alto: target "bad" unicode carefully
    No longer removes non-printable unicode because there is ambiguous
    unicode (soft hyphen) that is not considered printable, but becomes
    printable when it breaks up a word....

--- src/derivatives/alto/pdftotext.go
+++ src/derivatives/alto/pdftotext.go
@@ -132,9 +132,10 @@ func (w Word) clean() Word {
 	var cleaned = w
 	cleaned.Text = ""
 	for _, r := range []rune(w.Text) {
-		if unicode.IsPrint(r) {
-			cleaned.Text = cleaned.Text + string(r)
+		if unicode.In(r, unicode.Cc, unicode.Co, unicode.Cs) {
+			continue
 		}
+		cleaned.Text = cleaned.Text + string(r)
 	}
 
 	return cleaned
