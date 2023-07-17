7de81ce347dbf04d1417e90e93d4c9cd0d5e7768
Jeremy Echols <jechols@uoregon.edu>
2023-07-17 07:12:45 -0700

    Finalize pipeline migration
    Adds check to prevent migration when there are unfinished jobs, and
    fixes format of "down" error message.

--- src/cmd/migrate-database/migrations/20230626060501_add_pipelines.go
+++ src/cmd/migrate-database/migrations/20230626060501_add_pipelines.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/pressly/goose/v3"
+	"github.com/uoregon-libraries/newspaper-curation-app/src/models"
 )
 
 //go:embed "add_pipelines.sql"
@@ -33,7 +34,19 @@ func runStatements(tx *sql.Tx, stmts []string) error {
 }
 
 func upAddPipelines(tx *sql.Tx) error {
-	// TODO: check for non-closed jobs and exit if any
+	var res, err = tx.Exec("SELECT COUNT(id) FROM jobs WHERE status NOT IN (?, ?)",
+		models.JobStatusSuccessful, models.JobStatusFailedDone)
+	var count int64
+	if err == nil {
+		count, err = res.RowsAffected()
+		if count > 0 {
+			err = fmt.Errorf("cannot migrate until all jobs are completed or canceled")
+		}
+	}
+
+	if err != nil {
+		return fmt.Errorf("attempting to get count of unfinished jobs: %w", err)
+	}
 
 	// Read in and process the pipelines sql
 	var statements = strings.Split(string(sqldata), ";")
@@ -41,5 +54,5 @@ func upAddPipelines(tx *sql.Tx) error {
 }
 
 func downAddPipelines(_ *sql.Tx) error {
-	return fmt.Errorf("Cannot migrate down from the Pipeline feature")
+	return fmt.Errorf("cannot migrate down from the Pipeline feature")
 }
