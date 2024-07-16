b1a5bc82ed3f92d33bc3d34481e5bb5b25ef5477
Jeremy Echols <jechols@uoregon.edu>
2024-07-16 09:14:22 -0700

    Rewrite actions.js without jQuery

--- static/js/actions.js
+++ static/js/actions.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', () => {
   var lists = document.querySelectorAll(".action-list");
   for (i = 0; i < lists.length; i++) {
     lists[i].scrollTop = lists[i].scrollHeight;
