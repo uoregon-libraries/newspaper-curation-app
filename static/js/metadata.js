17e3c1f0c29ce718c23db25ed709dc276622e5b8
Jeremy Echols <jechols@uoregon.edu>
2024-07-16 09:48:17 -0700

    Rewrite metadata.js with no jquery

--- static/js/metadata.js
+++ static/js/metadata.js
@@ -1,59 +1,78 @@
 function goToNextUnlabeledPage() {
-  var currentPage = parseInt($("#osd-image-number").text()) - 1;
-  for (var x = currentPage; x < totalPages; x++) {
+  const currentPage = parseInt(document.getElementById('osd-image-number').textContent) - 1;
+  for (let x = currentPage; x < totalPages; x++) {
     if (pageLabels[x] == null || pageLabels[x] == "") {
       osd.goToPage(x);
       return;
     }
   }
 
-  var x = currentPage + 1;
+  const x = currentPage + 1;
   osd.goToPage(x % totalPages);
 }
 
-$(document).ready(function() {
-  $("#page-label-form").submit(function(e) {
-    // Store the page label in the global array
-    var currentPage = parseInt($("#osd-image-number").text()) - 1;
-    pageLabels[currentPage] = $("#page-label").val();
-    $("#page-labels-csv").val(pageLabels.join("␟"));
+document.addEventListener('DOMContentLoaded', function() {
+  const pageLabelForm = document.getElementById('page-label-form');
+  const metadataForm = document.getElementById('metadata-form');
+  const pageLabel = document.getElementById('page-label');
+  const pageLabelsCSV = document.getElementById('page-labels-csv');
+  const saveDraft = document.getElementById('savedraft');
+  const saveQueue = document.getElementById('savequeue');
 
-    goToNextUnlabeledPage();
+  if (pageLabelForm != null) {
+    pageLabelForm.addEventListener('submit', function(e) {
+      const currentPage = parseInt(document.getElementById('osd-image-number').textContent) - 1;
+      pageLabels[currentPage] = pageLabel.value;
+      pageLabelsCSV.value = pageLabels.join('␟');
 
-    // Try to do an auto-save in the background so users don't lose data if
-    // they close the browser or something
-    var form = $("#metadata-form")
-    var data = form.serialize();
-    data += "&action=autosave";
-    $.post(form.attr("action"), data);
+      goToNextUnlabeledPage();
 
-    // Don't actually submit the page-label form
-    e.preventDefault();
-    return false;
-  });
+      // Try to do an auto-save in the background so users don't lose data if
+      // they close the browser or something
+      const formData = new FormData(metadataForm);
+      formData.append('action', 'autosave');
+      fetch(metadataForm.getAttribute('action'), {method: 'POST', body: formData});
 
-  // Make sure page labels are saved on main form submission
-  $("#metadata-form").submit(function(e) {
-    var currentPage = parseInt($("#osd-image-number").text()) - 1;
-    pageLabels[currentPage] = $("#page-label").val();
-    $("#page-labels-csv").val(pageLabels.join("␟"));
-  });
+      // Don't actually submit the page-label form
+      e.preventDefault();
+      return false;
+    });
 
-  // Don't validate when saving as a draft
-  $("#savedraft").on("click", function() {
-    $("#metadata-form").attr("novalidate", "novalidate");
-  });
+    // Make sure page labels are saved on main form submission
+    metadataForm.addEventListener('submit', function() {
+      const currentPage = parseInt(document.getElementById('osd-image-number').textContent) - 1;
+      pageLabels[currentPage] = pageLabel.value;
+      pageLabelsCSV.value = pageLabels.join('␟');
+    });
 
-  // Make sure validation is reset when queueing for review
-  $("#savequeue").on("click", function() {
-    $("#metadata-form").removeAttr("novalidate");
-  });
+    // Don't validate when saving as a draft
+    saveDraft.addEventListener('click', function() {
+      metadataForm.setAttribute('novalidate', 'novalidate');
+    });
 
-  osd.addHandler("page", function(data) {
-    $('#page-label').val(pageLabels[data.page]);
-    $('#page-label-text').text(pageLabels[data.page]);
+    // Make sure validation is reset when queueing for review
+    saveQueue.addEventListener('click', function() {
+      metadataForm.removeAttribute('novalidate');
+    });
+  }
+
+  osd.addHandler('page', function(data) {
+    if (pageLabel != null) {
+      if (pageLabels[data.page] == null) {
+        pageLabels[data.page] = "";
+      }
+      pageLabel.value = pageLabels[data.page];
+    }
+
+    const pageLabelText = document.getElementById('page-label-text');
+    if (pageLabelText != null) {
+      pageLabelText.textContent = pageLabels[data.page];
+    }
   });
 
   osd.goToPage(0);
-  $('#page-label').val(pageLabels[0]);
+
+  if (pageLabel != null) {
+    pageLabel.value = pageLabels[0];
+  }
 });
