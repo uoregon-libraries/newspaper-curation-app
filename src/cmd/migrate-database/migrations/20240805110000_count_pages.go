e7ac0ef97859318c17a1aacc159b8678a075dbfd
Jeremy Echols <jechols@uoregon.edu>
2024-08-22 09:39:54 -0700

    Add progress logging to page count migration
    Not sure why this is taking a few minutes to update <100k records, but
    progress logging helps at least show that something is happening.

--- src/cmd/migrate-database/migrations/20240805110000_count_pages.go
+++ src/cmd/migrate-database/migrations/20240805110000_count_pages.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"database/sql"
 	"fmt"
+	"log/slog"
 
 	"github.com/pressly/goose/v3"
 	"github.com/uoregon-libraries/newspaper-curation-app/src/models"
@@ -16,14 +17,26 @@ func init() {
 // load each issue and re-save it so it uses the model's built-in logic to
 // cache page counts
 func upCountIssuePages(_ *sql.Tx) error {
+	slog.Info("Counting all issues' pages")
+	slog.Info("Loading all models. This may take a while...")
 	var issues, err = models.Issues().AllowIgnored().Fetch()
 	if err != nil {
 		return fmt.Errorf("loading all issues: %w", err)
 	}
-	for _, i := range issues {
-		err = i.SaveWithoutAction()
+
+	slog.Info("Load complete.")
+	for i, issue := range issues {
+		var mod = 1000
+		if i < 1000 {
+			mod = 100
+		}
+
+		if i%mod == 0 {
+			slog.Info("Saving models...", "done", i, "remaining", len(issues)-i)
+		}
+		err = issue.SaveWithoutAction()
 		if err != nil {
-			return fmt.Errorf("saving issue %q: %w", i.Key(), err)
+			return fmt.Errorf("saving issue %q: %w", issue.Key(), err)
 		}
 	}
 	return nil
