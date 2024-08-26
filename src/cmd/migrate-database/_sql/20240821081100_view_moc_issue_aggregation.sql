c6a3998b4bd456a2c665dda71cdd27e821f0d125
Jeremy Echols <jechols@uoregon.edu>
2024-08-26 15:58:46 -0700

    Fix MOC Issue aggregation view
    Forgot to have it skip ignored issues... ouch.

--- src/cmd/migrate-database/_sql/20240821081100_view_moc_issue_aggregation.sql
+++ src/cmd/migrate-database/_sql/20240821081100_view_moc_issue_aggregation.sql
@@ -4,6 +4,7 @@ CREATE VIEW moc_issue_aggregation AS
   SELECT m.id AS id, m.code, m.name, i.workflow_step, COUNT(i.id) AS issue_count, SUM(i.page_count) AS total_pages
     FROM mocs m
     JOIN issues i ON (i.marc_org_code = m.code)
+    WHERE i.ignored = 0
     GROUP BY marc_org_code, workflow_step
     ORDER BY marc_org_code, workflow_step;
 
