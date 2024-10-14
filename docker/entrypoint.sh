9e3d4b70076e7ddc2cd65038d94124993990bb7e
Jeremy Echols <jechols@uoregon.edu>
2024-10-14 07:01:49 -0700

    docker: remove directory creation
    The necessary paths are now auto-created in NCA, and this list was
    slightly incorrect anyway

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,15 +16,6 @@ flock $lockfile -c "./bin/migrate-database -c ./settings up"
 echo "Get SFTPgo admin API key and store in NCA settings file"
 flock /mnt/news/get-sftpgo-api-key-running -c "SETTINGS_PATH=settings SFTPGO_ADMIN_LOGIN=admin SFTPGO_ADMIN_PASSWORD=password sftpgo/get_admin_api_key.sh --force"
 
-echo "Ensuring directories are present"
-source settings && mkdir -p $PDF_UPLOAD_PATH
-source settings && mkdir -p $SCAN_UPLOAD_PATH
-source settings && mkdir -p $ORIGINAL_PDF_BACKUP_PATH
-source settings && mkdir -p $PDF_PAGE_REVIEW_PATH
-source settings && mkdir -p $BATCH_OUTPUT_PATH
-source settings && mkdir -p $WORKFLOW_PATH
-source settings && mkdir -p $ERRORED_ISSUES_PATH
-
 echo 'Executing "'$@'"'
 cd /usr/local/nca
 exec $@
