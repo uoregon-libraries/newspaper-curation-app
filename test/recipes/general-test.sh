dedea640f95727a148510cd03d8add58c5fb32a2
Jeremy Echols <jechols@uoregon.edu>
2024-12-02 10:21:14 -0800

    test/recipes: add a pre-report final backup

--- test/recipes/general-test.sh
+++ test/recipes/general-test.sh
@@ -49,12 +49,24 @@ if [[ ! -d ./backup/02-$name ]]; then
   ./manage backup 02-$name
 else
   echo "Detected backup 02; skipping processing"
-  echo "Restoring backup 02 to begin step 03"
-  ./manage restore 02-$name
+  if [[ ! -d ./backup/03-$name ]]; then
+    echo "Restoring backup 02 to begin step 03"
+    ./manage restore 02-$name
+  fi
 fi
 
-finish_batches
+if [[ ! -d ./backup/03-$name ]]; then
+  wait_db
+  finish_batches
 
+  ./manage backup 03-$name
+else
+  echo "Detected backup 03; skipping processing"
+  echo "Restoring backup 03 and writing report"
+  ./manage restore 03-$name
+fi
+
+wait_db
 cd test
 ./report.sh $name
 cd ..
