9e656850979056bf0ee5758ce8451e8b9a6a807a
Jeremy Echols <jechols@uoregon.edu>
2024-12-19 14:28:59 -0800

    undo

---
+++ src/cmd/server/internal/batchhandler/live_correct_handlers.go
@@ -0,0 +1,9 @@
+package batchhandler
+
+import "net/http"
+
+func flagLiveBatchFormHandler(w http.ResponseWriter, req *http.Request) {
+}
+
+func flagLiveBatchHandler(w http.ResponseWriter, req *http.Request) {
+}
