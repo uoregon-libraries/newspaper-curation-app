b36607091d9bbb335744f3b58eaf2a5020e5327c
Jeremy Echols <jechols@uoregon.edu>
2025-05-09 15:39:25 -0700

    Implement UI for finding live issues

---
+++ src/cmd/server/internal/managelivehandler/json_handlers.go
@@ -0,0 +1,122 @@
+package managelivehandler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/uoregon-libraries/newspaper-curation-app/internal/logger"
+	"github.com/uoregon-libraries/newspaper-curation-app/src/cmd/server/internal/responder"
+	"github.com/uoregon-libraries/newspaper-curation-app/src/duration"
+	"github.com/uoregon-libraries/newspaper-curation-app/src/models"
+)
+
+type jsonIssue struct {
+	*models.FlatIssue
+	FullTitle   string
+	WentLiveAt  string
+	PublishedOn string
+}
+
+const dateFmt = "Mon Jan _2, 2006"
+
+func wrapIssue(i *models.FlatIssue) *jsonIssue {
+	var ji = &jsonIssue{FlatIssue: i, PublishedOn: i.Date}
+	var pubd, err = time.Parse("2006-01-02", i.Date)
+	if err == nil {
+		ji.PublishedOn = pubd.Format(dateFmt)
+	}
+	ji.WentLiveAt = i.WentLiveAt.Format(dateFmt)
+	ji.FullTitle = fmt.Sprintf("%s (%s)", i.TitleName, i.LCCN)
+
+	return ji
+}
+
+type jsonResponse struct {
+	Code         int
+	Message      string
+	Issues       []*jsonIssue
+	TotalResults uint64
+}
+
+// jsonHandler produces a JSON feed of issue information to enable
+// rendering a subset of issues
+func jsonHandler(w http.ResponseWriter, req *http.Request) {
+	var r = responder.Response(w, req)
+	var response, err = getJSONIssues(r)
+	if err != nil {
+		logger.Errorf("Unable to get JSON issues: %s", err)
+		r.Writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(r.Writer, `{"Code": %d, "Message": %q}`, http.StatusInternalServerError, "Unable to retrieve issues from the database! Try again or contact support.")
+		return
+	}
+
+	var data []byte
+	data, err = json.Marshal(response)
+	if err != nil {
+		r.Writer.WriteHeader(http.StatusInternalServerError)
+		logger.CriticalFixNeeded(fmt.Sprintf("Unable to marshal %#v", response), err)
+		fmt.Fprintf(r.Writer, `{"Code": %d, "Message": %q}`, http.StatusInternalServerError, "Unable to retrieve issues from the database! Try again or contact support.")
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(response.Code)
+
+	// Ignore the Write error here - a client disconnecting mid-write causes an
+	// error which we do not care about
+	_, _ = w.Write(data)
+}
+
+func getJSONIssues(resp *responder.Responder) (*jsonResponse, error) {
+	var err error
+	var response = &jsonResponse{Code: http.StatusOK}
+
+	// Build filter function map to prepare our flat issue finder
+	var finder = models.FlatIssues().Live()
+	var filterMap = map[string]func(string) *models.FlatIssueFinder{
+		"moc":  finder.MOC,
+		"lccn": finder.LCCN,
+		"went-live": func(val string) *models.FlatIssueFinder {
+			var d, err = duration.Parse(val)
+			// Errors shouldn't be possible unless somebody hacks the form, so we
+			// just pretend the "went live" filter wasn't there
+			if err != nil {
+				return finder
+			}
+			var now = time.Now()
+			var then = d.SubtractFromTime(now)
+			return finder.WentLiveBetween(then, now)
+		},
+	}
+
+	// Apply filters based on request parameters
+	resp.Request.ParseForm()
+	for key, applyFilter := range filterMap {
+		var value = resp.Request.FormValue(key)
+		if value != "" {
+			finder = applyFilter(value)
+		}
+	}
+
+	response.TotalResults, err = finder.Count()
+	if err != nil {
+		logger.Errorf("Error counting issues in live-issue JSON handler: %s", err)
+		return nil, err
+	}
+
+	var issues []*models.FlatIssue
+	issues, err = finder.Limit(100).Fetch()
+	if err != nil {
+		logger.Errorf("Error reading issues in live-issue JSON handler: %s", err)
+		return nil, err
+	}
+
+	response.Issues = make([]*jsonIssue, len(issues))
+	for i, issue := range issues {
+		response.Issues[i] = wrapIssue(issue)
+	}
+
+	return response, nil
+}
---
+++ static/js/manage-live-issues.js
@@ -0,0 +1,139 @@
+(function() {
+  'use strict';
+
+  const filterParams = ['lccn', 'moc', 'went-live'];
+  let statusFadeTimeout = null;
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    // If args are present, pre-fill the form and fetch issues
+    let u = new URL(window.location);
+    let srch = new URLSearchParams(u.search.substr(1));
+    let filtersPresent = false;
+    for (const param of filterParams) {
+      const value = srch.get(param);
+      document.getElementById(param).value = value;
+      if (value) {
+        filtersPresent = true;
+      }
+    }
+    if (filtersPresent) {
+      loadIssues();
+    }
+
+    // Set up the filter form to fetch JSON from the server on submit
+    document.getElementById('filter-form').addEventListener('submit', fetchIssues);
+  });
+
+  function fetchIssues(e) {
+    e.preventDefault();
+    let u = new URL(window.location);
+    let srch = new URLSearchParams(u.search.substr(1));
+    for (const param of filterParams) {
+      srch.set(param, document.getElementById(param).value);
+    }
+    u.search = srch.toString();
+    history.replaceState(null, '', u);
+    loadIssues();
+  }
+
+  async function loadIssues() {
+    const srchMessage = document.getElementById('search-results-message');
+    const table = document.getElementById('search-results-table');
+    const statusDiv = document.getElementById('json-status');
+
+    // Cancel any pending fade-out of the status message
+    if (statusFadeTimeout) {
+      clearTimeout(statusFadeTimeout);
+      statusFadeTimeout = null;
+    }
+
+    // Clear current search results and show loading status
+    srchMessage.innerHTML = '<p><em>Loading...</em></p>';
+    srchMessage.style.display = 'block';
+    table.style.display = 'none';
+
+    let loading = setTimeout(() => {
+      statusDiv.setAttribute('class', 'inline alert alert-info');
+      statusDiv.dataset.faded = false;
+      statusDiv.innerText = 'Fetching issues from server...';
+    }, 200);
+
+    let u = new URL(window.location);
+    let srch = new URLSearchParams(u.search.substr(1));
+
+    let searchURL = document.getElementById('filter-form').dataset.url;
+    var response, data;
+    try {
+      response = await fetch(searchURL + '?' + srch.toString());
+      data = await response.json();
+    }
+    catch (e) {
+      console.error('Exception caught during fetch: ', e);
+      statusDiv.setAttribute('class', 'inline alert alert-danger');
+      statusDiv.innerText = 'Network error trying to retrieve issues: please reload the page and try again, or contact support.';
+      srchMessage.innerHTML = '<p><em>Error loading issues.</em></p>';
+      srchMessage.style.display = 'block';
+      table.style.display = 'none';
+      return;
+    }
+
+    if (!response.ok) {
+      statusDiv.setAttribute('class', 'inline alert alert-warning');
+      statusDiv.innerText = data.Message;
+      srchMessage.innerHTML = '<p><em>Error loading issues.</em></p>';
+      srchMessage.style.display = 'block';
+      table.style.display = 'none';
+      return;
+    }
+
+    clearTimeout(loading);
+    statusDiv.setAttribute('class', 'inline alert');
+    statusDiv.innerText = `Load complete: ${data.TotalResults} issues matched.`;
+    statusFadeTimeout = setTimeout(() => {
+      statusDiv.dataset.faded = true;
+      statusFadeTimeout = null;
+    }, 5000);
+
+    populateSearchResults(data.Issues, data.TotalResults);
+  }
+
+  function populateSearchResults(issues, totalResults) {
+    const srchMessage = document.getElementById('search-results-message');
+    const table = document.getElementById('search-results-table');
+    const tbody = table.tBodies[0];
+    const tcaption = table.caption;
+
+    tbody.innerHTML = '';
+
+    if (issues == null || issues.length === 0) {
+      srchMessage.innerHTML = '<p><em>No issues found matching your criteria.</em></p>';
+      srchMessage.style.display = 'block';
+      table.style.display = 'none';
+      return;
+    }
+
+    srchMessage.style.display = 'none';
+    table.style.display = 'table';
+
+    if (issues.length < totalResults) {
+      tcaption.textContent = `Search results: too many matches; showing ${issues.length} (of ${totalResults}).`;
+    } else {
+      tcaption.textContent = `Search results: ${issues.length} matches`;
+    }
+
+    for (const issue of issues) {
+      const row = tbody.insertRow();
+
+      row.insertCell().textContent = issue.FullTitle;
+      row.insertCell().textContent = issue.PublishedOn + ", ed. " + issue.Edition;
+      row.insertCell().textContent = issue.BatchName;
+      row.insertCell().textContent = issue.WentLiveAt;
+
+      const cell = row.insertCell();
+      const link = document.createElement('a');
+      link.innerHTML = 'Remove...';
+      link.setAttribute('class', 'btn btn-danger');
+      cell.appendChild(link);
+    }
+  }
+})();
