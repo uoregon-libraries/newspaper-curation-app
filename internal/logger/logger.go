3a938f08929967c74861c8e1c45c01c7a190bc0b
Jeremy Echols <jechols@uoregon.edu>
2025-05-20 09:25:41 -0700

    internal/logger: create meaningful docs
    Explicitly adds information to each log level function to explain when
    NCA should be using them, and what they mean

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -16,32 +16,45 @@ import (
 // change the log level or otherwise customize it, this can be overwritten.
 var Logger = l.New(l.Debug, false)
 
-// Debugf logs a debug-level message
+// Debugf logs a debug-level message. This should be disabled in production
+// except when... well... debugging.
 func Debugf(format string, args ...any) {
 	Logger.Debugf(fmt.Sprintf(format, args...))
 }
 
-// Infof logs an info-level message
+// Infof logs an info-level message. These should just give general info that
+// helps uncover problems or tells us a train of some process we sometimes need
+// to manually check.
 func Infof(format string, args ...any) {
 	Logger.Infof(fmt.Sprintf(format, args...))
 }
 
-// Warnf logs a warn-level message
+// Warnf logs a warn-level message. Use this when something goes wrong, but
+// doesn't really cause any problems, or the problems caused aren't something
+// that realistically can be addressed in code.
 func Warnf(format string, args ...any) {
 	Logger.Warnf(fmt.Sprintf(format, args...))
 }
 
-// Errorf logs an error-level message
+// Errorf logs an error-level message. This should be used for things that we
+// don't expect, and need to fix. The current code is overusing this log level,
+// and should be looked at.
 func Errorf(format string, args ...any) {
 	Logger.Errorf(fmt.Sprintf(format, args...))
 }
 
-// Criticalf logs a critical-level message
+// Criticalf logs a critical-level message. We should only use these when
+// something goes so badly wrong that dev intervention is likely necessary to
+// fix issues, or something is just so unexpected that seeing this message
+// means some kind of attention is needed somewhere ASAP.
 func Criticalf(format string, args ...any) {
 	Logger.Criticalf(fmt.Sprintf(format, args...))
 }
 
-// Fatalf logs a critical-level message, then exits
+// Fatalf logs a critical-level message, then exits. The same rules apply here
+// as [Criticalf], this just lets us stop a command mid-run. Obviously
+// shouldn't be used in any of the daemons unless something just unbelievably
+// bad happens.
 func Fatalf(format string, args ...any) {
 	Logger.Fatalf(fmt.Sprintf(format, args...))
 	os.Exit(1)
