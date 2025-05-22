d34f4a71b878b5a5cc98612a1b3b55922b436ebb
Jeremy Echols <jechols@uoregon.edu>
2025-05-22 08:03:45 -0700

    retry: refactor to allow testing

--- internal/retry/retry.go
+++ internal/retry/retry.go
@@ -19,6 +19,20 @@ const (
 	Multiplier = 1.25
 )
 
+// timeProvider allows for dependency injection of time functions
+type timeProvider interface {
+	Now() time.Time
+	Sleep(time.Duration)
+}
+
+// realTimeProvider implements TimeProvider using real time functions
+type realTimeProvider struct{}
+
+func (realTimeProvider) Now() time.Time        { return time.Now() }
+func (realTimeProvider) Sleep(d time.Duration) { time.Sleep(d) }
+
+var defaultTimeProvider timeProvider = realTimeProvider{}
+
 // Do attempts to run the given function repeatedly until it succeeds or
 // maxWait has expired. Starting at [InitialDelay], each failure increases the
 // delay between it and the next try by a factor of [Multiplier], up to a
@@ -33,22 +47,26 @@ const (
 // already done. Only use retry.Do when the operation is idempotent or the risk
 // of failure is worse than the damage done by repeating an operation.
 func Do(maxWait time.Duration, fn func() error) error {
+	return doWithTimeProvider(maxWait, fn, defaultTimeProvider)
+}
+
+// doWithTimeProvider is the same as Do but allows injecting a custom time provider for testing
+func doWithTimeProvider(maxWait time.Duration, fn func() error, tp timeProvider) error {
 	var err error
 	var delay = InitialDelay
-	var start = time.Now()
-	for time.Since(start) < maxWait {
+	var start = tp.Now()
+
+	for tp.Now().Sub(start) < maxWait {
 		err = fn()
 		if err == nil {
 			return nil
 		}
 
-		time.Sleep(delay)
-
+		tp.Sleep(delay)
 		delay = time.Duration(float64(delay) * Multiplier)
 		if delay > MaxDelay {
 			delay = MaxDelay
 		}
 	}
-
 	return fn()
 }
